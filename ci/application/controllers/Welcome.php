00abcc5c0d4fb485fc50db04835e5153739c301f
zrhth5 <zrhth5@mail.missouri.edu>
2017-02-12 19:15:31 -0600

    Fixed PHP Error

--- ci/application/controllers/Welcome.php
+++ ci/application/controllers/Welcome.php
@@ -4,7 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
     
     function __construct(){
-        parent::__construct();
         $this->load->model('UserAccountModel');
     }
     
