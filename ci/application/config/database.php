266146ebce9519457019c1b3c25c5f2c76482585
zrhth5 <zrhth5@mail.missouri.edu>
2017-02-12 19:49:09 -0600

    Testing Database Connection

--- ci/application/config/database.php
+++ ci/application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'zach',
 	'password' => 'Capstone17!',
 	'database' => 'umb',
-	'dbdriver' => 'mysql',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
