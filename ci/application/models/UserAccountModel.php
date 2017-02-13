8668d6baa621ed24350a023149b69a4016eb1336
zrhth5 <zrhth5@mail.missouri.edu>
2017-02-12 19:36:38 -0600

    Testing Database Connection

--- ci/application/models/UserAccountModel.php
+++ ci/application/models/UserAccountModel.php
@@ -3,7 +3,7 @@ class UserAccountModel extends CI_Model {
     
     function getUserAccounts(){
         
-        $this->load->database();
+        $this->db = $this->load->database();
         $this->db->select("user_id, email, first_name, last_name, enabled");
         $this->db->from('UserAccount');
         $query = $this->db->get();
