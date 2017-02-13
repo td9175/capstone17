e945db918ef24e32d8f75d317a5032e19ac1e6b4
zrhth5 <zrhth5@mail.missouri.edu>
2017-02-12 19:25:42 -0600

    Fixed PHP Error

--- ci/application/models/UserAccountModel.php
+++ ci/application/models/UserAccountModel.php
@@ -2,6 +2,8 @@
 class UserAccountModel extends CI_Model {
     
     function getUserAccounts(){
+        
+        $this->load->database();
         $this->db->select("user_id, email, first_name, last_name, enabled");
         $this->db->from('UserAccount');
         $query = $this->db->get();
