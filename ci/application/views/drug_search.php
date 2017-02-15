b721eceae426d1162f761a16eaf4980fd04ebe99
rwfwcb <rwfwcb@mail.missouri.edu>
2017-02-14 22:32:48 -0600

    Fixed errors

--- ci/application/views/drug_search.php
+++ ci/application/views/drug_search.php
@@ -1,6 +1,6 @@
 <?php
 
-echo form_open('DrugSearch/search_for_drug');
+echo form_open(site_url("DrugSearch/search_for_drug"));
 echo form_input('searchQuery', set_value('searchQuery'));
 echo form_submit('drugSearchSubmit', 'Search!');
 echo form_close();
