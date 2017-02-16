c95abedd26a6632b0f0156a4fe25a224abb5f3ed
skh6yf <skh6yf@mail.missouri.edu>
2017-02-15 20:28:00 -0600

    db testing

--- ci/application/models/UserAccountModel.php
+++ ci/application/models/UserAccountModel.php
@@ -5,11 +5,20 @@ class UserAccountModel extends CI_Model {
         
         #$this->db = 
         $this->load->database();
-        $query = $this->db->query('SELECT user_id FROM UserAccount');
+		
+		
+        
+        
+        $query = $this->db->query('SELECT user_id FROM UserAccount LIMIT 1');
+		$row = $query->row();
+		echo $row->name;
+		return $row->name;
+        
+        
         #$this->db->select("user_id, email, first_name, last_name, enabled");
         #$this->db->from('UserAccount');
         #$query = $this->db->get();
-        return $query->result();
+        #return $query->result();
     }
     
 }
