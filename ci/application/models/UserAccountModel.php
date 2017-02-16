4575de6cd5971864d50b78a1c61c1da97ebf6740
skh6yf <skh6yf@mail.missouri.edu>
2017-02-16 10:49:10 -0600

    changing to print all results from db

--- ci/application/models/UserAccountModel.php
+++ ci/application/models/UserAccountModel.php
@@ -6,7 +6,21 @@ class UserAccountModel extends CI_Model {
       
         $this->load->database();
 
-        $query = $this->db->query('SELECT user_id FROM UserAccount LIMIT 1');
+        $query = $this->db->query('SELECT * from UserAccount');
+        
+        
+        foreach ($query->result_array() as $row){
+        echo $row['user_id'];
+        echo $row['email'];
+        echo $row['hash_pass'];
+        echo $row['first_name'];
+        echo $row['last_name'];
+        echo $row['enabled'];
+		}
+        
+        
+        
+        
 		$row = $query->row();
 		echo $row->user_id;
 		return $row->user_id;
