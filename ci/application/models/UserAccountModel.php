57f48c8b416da91bae7a14874d0eddff54097040
skh6yf <skh6yf@mail.missouri.edu>
2017-02-15 20:17:55 -0600

    Remove quotes from query

--- ci/application/models/UserAccountModel.php
+++ ci/application/models/UserAccountModel.php
@@ -4,7 +4,7 @@ class UserAccountModel extends CI_Model {
     function getUserAccounts(){
         
         $this->db = $this->load->database();
-        $query = $this->db->query("SELECT 'user_id' FROM UserAccount");
+        $query = $this->db->query("SELECT user_id FROM UserAccount");
         #$this->db->select("user_id, email, first_name, last_name, enabled");
         #$this->db->from('UserAccount');
         #$query = $this->db->get();
