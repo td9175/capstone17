eeddc984189c5b72926ff14ebadf0ac85a846f4c
skh6yf <skh6yf@mail.missouri.edu>
2017-02-15 20:12:10 -0600

    fixing DB query

--- ci/application/models/UserAccountModel.php
+++ ci/application/models/UserAccountModel.php
@@ -4,9 +4,10 @@ class UserAccountModel extends CI_Model {
     function getUserAccounts(){
         
         $this->db = $this->load->database();
-        $this->db->select("user_id, email, first_name, last_name, enabled");
-        $this->db->from('UserAccount');
-        $query = $this->db->get();
+        $query = $this->db->query("SELECT 'user_id' FROM UserAccount");
+        #$this->db->select("user_id, email, first_name, last_name, enabled");
+        #$this->db->from('UserAccount');
+        #$query = $this->db->get();
         return $query->result();
     }
     
