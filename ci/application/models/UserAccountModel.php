ecdc8ae33c9b46cd04adb4174631e33b2d6db398
skh6yf <skh6yf@mail.missouri.edu>
2017-02-15 20:22:24 -0600

    again w the quotes

--- ci/application/models/UserAccountModel.php
+++ ci/application/models/UserAccountModel.php
@@ -3,7 +3,8 @@ class UserAccountModel extends CI_Model {
     
     function getUserAccounts(){
         
-        $this->db = $this->load->database();
+        #$this->db = 
+        $this->load->database();
         $query = $this->db->query('SELECT user_id FROM UserAccount');
         #$this->db->select("user_id, email, first_name, last_name, enabled");
         #$this->db->from('UserAccount');
