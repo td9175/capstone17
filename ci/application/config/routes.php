f1a23889a49e1f1ec921d197d7dfed78517894e4
rwfwcb <rwfwcb@mail.missouri.edu>
2017-02-17 19:25:04 -0600

    Fixing drug search
    Added route

--- ci/application/config/routes.php
+++ ci/application/config/routes.php
@@ -52,3 +52,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['drugsearch/search_for_drug'] = 'drugsearch/search_for_drug';
