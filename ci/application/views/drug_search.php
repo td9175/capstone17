cf29f5d454bb1a7f3a1d4dedb8aab779b425b5a3
rwfwcb <rwfwcb@mail.missouri.edu>
2017-02-18 21:24:09 -0600

    Fixing errors

--- ci/application/views/drug_search.php
+++ ci/application/views/drug_search.php
@@ -1,6 +1,6 @@
 
 <?php echo form_open('DrugSearch/search_for_drug'); ?>
-	<label for='searchQuery'>Enter a drug to search for: </label>
+	<label for='searchQuery'>Enter a drug to search: </label>
 	<input type='text' name='searchQuery'>
 	<input type='submit' name='submit' value='Search!'>
 	</form>
@@ -14,7 +14,7 @@ if (isset($response)){
 	
 	$candidates = json_decode($response);
 	
-	print "Count: " . count($candidates);
+	print "Count: " . count($candidates) . "\n";
 	
 	var_dump($candidates);
 	
