c8ac719bfd706d447ea73f0a59677439f411cfdf
rwfwcb <rwfwcb@mail.missouri.edu>
2017-02-18 18:31:02 -0600

    Fixing drug search

--- ci/application/views/drug_search.php
+++ ci/application/views/drug_search.php
@@ -1,6 +1,6 @@
 
 <?php echo form_open('DrugSearch/search_for_drug'); ?>
-	<label for='searchQuery'>Enter a drug to search for: </label>
+	<label for='searchQuery'>Enter a drug to search: </label>
 	<input type='text' name='searchQuery'>
 	<input type='submit' name='submit' value='Search!'>
 	</form>
@@ -11,7 +11,12 @@
 if (isset($response)){
 	print $response;
 	print "\n";
-	
+	foreach ($response->candidates as $candidate )
+	{
+		print $candidate;
+		print "!!!\n";
+	}
+		
 }
 
 ?>
