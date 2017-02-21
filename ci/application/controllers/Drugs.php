d3396edad1a3762ffba383a25670fed104622123
rwfwcb <rwfwcb@mail.missouri.edu>
2017-02-21 16:23:03 -0600

    Fixing get errors

--- ci/application/controllers/Drugs.php
+++ ci/application/controllers/Drugs.php
@@ -64,10 +64,10 @@ class Drugs extends CI_Controller {
       $response = json_decode($response);
 
       // Insert the response object into the data array    
-      $data['searchData'] = $response;
+      //$data['searchData'] = $response;
       
       // Load the drugs view and pass the data array along with it
-      $this->load->view('drugs', $data);
+      //$this->load->view('drugs', $data);
     }
     
     
