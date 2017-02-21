ed2ea04b38f75f242978fe714ce081a1abeae6c0
skh6yf <skh6yf@mail.missouri.edu>
2017-02-20 18:30:31 -0600

    testing

--- ci/application/models/UserAccountModel.php
+++ ci/application/models/UserAccountModel.php
@@ -8,21 +8,9 @@ class UserAccountModel extends CI_Model {
 
         $query = $this->db->query('SELECT * from UserAccount');
         
-        $results = array();
-        foreach ($query->result_array() as $row){
-        $results['user_id'] = $row['user_id'];
-        $results['email'] = $row['email'];
-        $results['hash_pass'] = $row['hash_pass'];
-        $results['first_name'] = $row['first_name'];
-        $results['last_name'] = $row['last_name'];
-        //echo $row['email'];
-        //echo $row['hash_pass'];
-        //echo $row['first_name'];
-        //echo $row['last_name'];
-        //echo $row['enabled'];
-		}
-
-		return $results;
+		$row = $query->row();
+		
+		return $row->user_id;
         
        
     }
