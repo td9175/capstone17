7b6f624c38685a69e8763f2bbadc5576c29f1152
skh6yf <skh6yf@mail.missouri.edu>
2017-02-27 22:38:26 -0600

    update query

--- ci/application/models/UserAccountModel.php
+++ ci/application/models/UserAccountModel.php
@@ -5,24 +5,19 @@ class UserAccountModel extends CI_Model {
     
     function get_user_id() {
     	$this->load->database();
-        echo "no here i am";
         $return_arr = array();
 
-		//$fetch = mysql_query("SELECT * FROM table");
-		 $query = $this->db->query('SELECT * from UserAccount');
 		 
 		 foreach ($query->result_array() as $row)
 		{
-        	echo $row['user_id'];
-        	echo $row['email'];
-        	echo $row['first_name'];
-        	//$row['user_id'] = $return_arr['user_id'];
-        	//$row['email'] = $return_arr['email'];
-        	//$row['first_name'] = $return_arr['first_name'];
+        	//echo $row['user_id'];
+        	//echo $row['email'];
+        	//echo $row['first_name'];
         	
         	$data[] = array(
-				'user_id' => $row['user_id']
-				
+				'user_id' => $row['user_id'],
+				'email' => $row['email'],
+				'first_name' => $row['first_name']
 				);
         	
         	
