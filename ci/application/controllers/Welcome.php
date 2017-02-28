23fca4ef0da819d7f1ee3ac799abc9975a29598c
skh6yf <skh6yf@mail.missouri.edu>
2017-02-27 22:07:17 -0600

    reverse reverse

--- ci/application/controllers/Welcome.php
+++ ci/application/controllers/Welcome.php
@@ -24,7 +24,13 @@ class Welcome extends Rest_Controller {
 		$this->load->view('welcome_message');
 	}
 	
-	
+	public function index_get()
+    {
+    	echo "user get()";
+    	//call would be = index.php/Welcome/user?id=1
+        $data = array('returned: '. $this->get('id'));
+        $this->response($data);
+    }
 }
 
 ?>
