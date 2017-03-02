4d6cb92c183d87832213f01eeac50bc507bbd820
skh6yf <skh6yf@mail.missouri.edu>
2017-03-02 10:54:29 -0600

    fix

--- ci/application/controllers/Rest.php
+++ ci/application/controllers/Rest.php
@@ -1,7 +1,7 @@
 <?php
 require('application/libraries/REST_Controller.php');
  
-	class UserAccount extends REST_Controller {
+	class Rest extends REST_Controller {
 		
 		public function __construct() {
 			parent::__construct();
