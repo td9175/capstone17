2030d203318e1835f7db4994e7c698e6eee2b217
skh6yf <skh6yf@mail.missouri.edu>
2017-03-02 13:28:01 -0600

    update

--- ci/application/controllers/Rest.php
+++ ci/application/controllers/Rest.php
@@ -3,20 +3,39 @@ require('application/libraries/REST_Controller.php');
  
 	class Rest extends REST_Controller {
 		
-		public function __construct() {
-			parent::__construct();
-			$this->load->model('UserAccountModel');
-			echo "ello"; 
+		function user_get() {
+		//index.php/rest/user/id/1/format/json
+		//"id/1" is the parameter 
+        // respond with information about a user
+    	}
+     
+    	function users_get() {
+        // respond with information about several users
+        //index.php/rest/users
+        
+        $this->load->model('UserAccountModel');
 		
-		}
-		
-		
-		public function index() {
-			echo "index";
-			
-		
-		}
-	
+			if(!$this->get('id'))
+			{
+				$this->response(NULL, 400);
+			}
+ 			
+			$users = $this->UserAccountModel->get_user_id();
+		 
+			if($users)
+			{
+				$this->response($user, 200); // 200 being the HTTP response code
+			}
+ 
+			else
+			{
+				$this->response(NULL, 404);
+			}
+        
+        
+        
+        
+    	}
 		
  
 	}
