eace953b817cd965d9ea1655bc08bd8cbf8b4e66
skh6yf <skh6yf@mail.missouri.edu>
2017-03-03 16:17:05 -0600

    idk

--- ci/application/views/view_form.php
+++ ci/application/views/view_form.php
@@ -1,23 +1,24 @@
-<html>
+<?php 
+echo"<html>
     <head>		
         <title>Codeigniter Form Submit</title>
         <!--
         <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Open+Sans+Condensed:300|Raleway' rel='stylesheet' type='text/css'>
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        -->
-    </head>
+        <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
+       
+    </head>";
     
-    	<?php echo form_open('form/data_submitted');
+    echo form_open('form/data_submitted');
     			echo form_label('User Name :', 'u_name');
-    			?>
+    			
     	
-    <body> 
-        <div class="main">
-            <div id="content">
+    echo "<body> 
+        <div class='main'>
+            <div id='content'>
                 <h3 id='form_head'>Codelgniter Form Submit With Database</h3><br/>
                 <hr>
-                <div id="form_input">
-                <?php
+                <div id='form_input'>";
+               
                 echo form_open('form/data_submitted');
                 echo form_label('User Name :', 'u_name');
                 $input1_data = array(
@@ -35,26 +36,16 @@
                     'class' => 'input_box'
                 );
                 echo form_input($input2_data);
-                ?>
-                    </div>
-                    <div id="form_button">
-                        <?php echo form_submit('submit', 'Submit', "class='submit'"); ?>
-                    </div>                   
-               <?php echo form_close();?>
-                <?php if(isset($user_name) && isset($user_email_id)){
-                    echo "<div id='content_result'>";
-                    echo "<h3 id='result_id'>You have submitted these values</h3><br/><hr>";
-                    echo "<div id='result_show'>";
-                    echo "<label class='label_output'>Entered User Name : </label>".$user_name;
-                    echo"<br/><br/>";
-                    echo "<label class='label_output'>Entered Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: </label>".$user_email_id;
-                    echo "<div>";
-                    echo "</div>";
-                } ?>
+              
+            
+              echo"      </div>
+                    
             </div> 
         </div>
     </body>
-</html>
+</html>";
+
+?>
 
 
 
