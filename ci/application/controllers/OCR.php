bd52be4acc25b33739f03e6718a451794fd45a74
skh6yf <skh6yf@mail.missouri.edu>
2017-03-03 20:08:23 -0600

    testing new OCR api

--- ci/application/controllers/OCR.php
+++ ci/application/controllers/OCR.php
@@ -2,79 +2,15 @@
 //defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Ocr extends CI_Controller {
+	    
+	    
+	    public function testRecognizingTextFromImage() {
+        $expected = "The quick brown fox\njumps over the lazy\ndog.";
+
+        $actual = (new TesseractOCR('/receipts/test.png'))->run();
+
+        $this->assertEquals($expected, $actual);
+    	}
 
 
-
-
-  	public function __construct()
-	{
-        parent::__construct();
-        //$this->config->load('goodRx');
-        //$this->load->helper('url');
-		
-    }
-	public function index() {
-		$this->load->helper('form');
-		$this->load->view('ocr');
-	}
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-
-
-
-	public function getImage($img){
-
-
-=======
->>>>>>> Stashed changes
-
-
-	public function getImage(){
-
-		echo "here i am";
-<<<<<<< Updated upstream
-=======
->>>>>>> origin/master
->>>>>>> Stashed changes
-			//extract data from the post
-		//set POST variables
-		$url = 'https://westus.api.cognitive.microsoft.com/vision/v1.0/ocr?language=en&detectOrientation =true';
-		$this->headers[] = 'Content-type: application/json';
-		$this->headers[] = 'Ocp-Apim-Subscription-Key: 16eb25ebbaeb430695f63f2b23f22606';
-		$this->headers[] = 'Host: westus.api.cognitive.microsoft.com';
-
-
-		$body = '{"url":"http://example.com/images/test.jpg"}';
-
-		
-
-		//open connection
-		$ch = curl_init();
-
-		//set the url, number of POST vars, POST data
-		curl_setopt($ch,CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
-		curl_setopt($ch,CURLOPT_POST, count($fields));
-		curl_setopt($ch,CURLOPT_POSTFIELDS, $body);
-
-		//execute post
-		$result = curl_exec($ch);
-
-		//close connection
-		curl_close($ch);
-
-		}
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> Stashed changes
-
-
-
->>>>>>> origin/master
-	}
-
-?>
+}
