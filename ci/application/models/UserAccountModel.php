7d329d2166249e2f9463e315db5d88b5bd44e45f
rwfwcb <rwfwcb@mail.missouri.edu>
2017-03-05 13:21:45 -0600

    Fixing user post API

--- ci/application/models/UserAccountModel.php
+++ ci/application/models/UserAccountModel.php
@@ -46,26 +46,27 @@ class UserAccountModel extends CI_Model {
     	return $data;
     }
 
-    //function post_user($email, $hash_pass, $first_name, $last_name){
-    function post_user_email($email){
+    function post_user($email, $hash_pass, $first_name, $last_name){
+    //function post_user($email){
+
       // Load the database
       $this->load->database();
 
       // Sanitize the user input
-      //$email = $this->db->escape($email);
-      //$hash_pass = $this->db->escape($hash_pass);
-      //$first_name = $this->db->escape($first_name);
-      //$last_name = $this->db->escape($last_name);
+      $email = $this->db->escape($email);
+      $hash_pass = $this->db->escape($hash_pass);
+      $first_name = $this->db->escape($first_name);
+      $last_name = $this->db->escape($last_name);
 
       // Build the query to create a user account
       $query = "INSERT INTO UserAccount (email, hash_pass, first_name, last_name) VALUES (?,?,?,?)";
 
       // testing variables - make sure to delete this
-      $hash_pass = "password";
-      $first_name = "bobby";
-      $last_name = "fink";
+      // $hash_pass = "password";
+      // $first_name = "bobby";
+      // $last_name = "fink";
 
-      //Create an array of the parameters for paramater binding
+      // Create an array of the parameters for paramater binding
       $params = array($email, $hash_pass, $first_name, $last_name);
 
       // Execute the query
@@ -75,7 +76,7 @@ class UserAccountModel extends CI_Model {
         $data['registration'] = "Error! User registration failed.\n";
       }
 
-      //Pass back the data
+      // Pass back the data
       return $data;
 
     }
