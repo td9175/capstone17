112d4f1411fc9dc4634fe0340b635eb1406dc37e
rwfwcb <rwfwcb@mail.missouri.edu>
2017-03-05 15:17:35 -0600

    Fixing errors

--- ci/application/controllers/Registration.php
+++ ci/application/controllers/Registration.php
@@ -3,11 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Registration extends CI_Controller {
 
+/*
 	public function __construct()
 	{
-		    $this->load->helper('form');
-  }
 
+  }
+*/
 
 	public function index()
 	{
