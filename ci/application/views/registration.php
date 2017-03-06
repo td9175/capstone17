61244df8e4882f9a40af7d669ef8a55e8bef3243
rwfwcb <rwfwcb@mail.missouri.edu>
2017-03-05 20:41:45 -0600

    Fixing user registration api

--- ci/application/views/registration.php
+++ ci/application/views/registration.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 
-  <title>Register for UMB Health Spending App Now</title>
+  <title>UMB Health Spending App Registration</title>
 
   <!-- Load jQuery -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
@@ -12,16 +12,11 @@
 
         $("#submit_registration").value = "Loading";
 
-        // var email= $('#email').val();
-        // var password = $('#password').val();
-        // var password_check = $('#password_check').val();
-        // var first_name = $('#first_name').val();
-        // var last_name = $('#last_name').val();
-
-        console.log($('#email').val());
-        console.log($('#password').val());
-        console.log($('#first_name').val());
-        console.log($('#last_name').val());
+        var email= $('#email').val();
+        var password = $('#password').val();
+        var password_check = $('#password_check').val();
+        var first_name = $('#first_name').val();
+        var last_name = $('#last_name').val();
 
         var url = "http://capstone.td9175.com/ci/index.php/Rest/registration";
 
@@ -33,7 +28,7 @@
             last_name: $('#last_name').val()
           },
           function(data, status){
-              $("response").text = "Data: " + data + "\nStatus: " + status;
+              $("#response").text = "Data: " + data + "\nStatus: " + status;
           });
 
       });
@@ -44,49 +39,17 @@
 
 <body>
 
-  <h1>Register for UMB Health Spending App</h1>
+  <h1>Register for UMB Health Spending App Now!</h1>
 
   <input type='text' id='email' placeholder='Email address'><br><br>
-  <input type='text' id='password' placeholder='Password'><br><br>
-  <input type='text' id='password_check' placeholder='Password again'><br><br>
+  <input type='password' id='password' placeholder='Password'><br><br>
+  <input type='password' id='password_check' placeholder='Password again'><br><br>
   <input type='text' id='first_name' placeholder='First name'><br><br>
   <input type='text' id='last_name' placeholder='Last name'><br><br>
-  <button id='submit_registration'>Register now!</button>
+  <button id='submit_registration'>Register now!</button><br><br>
 
   <label id='response'></label>
-
 
 </body>
 
 </html>
-
-
-
-
-
-
-
-<!--
-
-<script>
-  function register_account() {
-
-    var email = document.getElementById('email');
-    var password = document.getElementById('password');
-    var password_check = document.getElementById('password_check');
-    var first_name = document.getElementById('first_name');
-    var last_name = document.getElementById('last_name');
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("demo").innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("POST", "Rest/registration/" + , true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send("email/" + email + "/hash_pass/" + password + "/first_name/" + first_name + "/last_name/" + last_name);
-  }
-</script>
-
--->
