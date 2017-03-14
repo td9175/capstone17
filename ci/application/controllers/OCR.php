d41955eb68e70285ec0f837a9ee7be2553ad3d85
skh6yf <skh6yf@mail.missouri.edu>
2017-03-14 14:27:54 -0500

    new our

--- ci/application/controllers/OCR.php
+++ ci/application/controllers/OCR.php
@@ -4,15 +4,30 @@
 class Ocr extends CI_Controller {
 	    
 	    
-	    public function testRecognizingTextFromImage() {
-        $expected = "The quick brown fox\njumps over the lazy\ndog.";
+        // Get cURL resource
+$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'https://api.ocr.space/Parse/Image',
+    CURLOPT_POST => 1,
+    CURLOPT_POSTFIELDS => array(
+        apikey => '26be4c08a388957',
+        language => 'eng',
+        url => 'https://c1.staticflickr.com/6/5309/5639277711_aac21b0e02_b.jpg'
+    )
+));
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
 
-        $actual = (new TesseractOCR('/receipts/test.png'))->run();
+echo "response: " . json_encode($resp);
+// Close request to clear up some resources
 
-        $this->assertEquals($expected, $actual);
-    	}
+curl_close($curl);
+public function index() {
+$this->load->view('ocr.php');
 
-
+}
 }
 
 ?>
