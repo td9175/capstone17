593fb3daa1f0ef349f7fba455c5d83e5f7a7d86e
skh6yf <skh6yf@mail.missouri.edu>
2017-03-14 19:39:50 -0500

    y

--- ci/application/models/AccountTransactionModel.php
+++ ci/application/models/AccountTransactionModel.php
@@ -2,34 +2,25 @@
 	class AccountTransactionModel extends CI_Model {
 
 	
-	function get_transaction_info($acct_num) {
-		
-		$this->load->database();
+    function get_trans_info($acct_num) {
+    	//http://capstone.td9175.com/ci/index.php/rest/user/id/1
+    	//request for a specfic id
 
-		 $query = "SELECT * FROM AccountTransaction WHERE account_number = ?";
+    	$this->load->database();
+
+    	$query = "SELECT * FROM AccountTransaction WHERE account_number = ?";
 
 		 $result = $this->db->query($query, $acct_num);
-$data = array();
-
-		 foreach ($result->result_array() as $row) {
-			
-        	$data[] = array(
+		foreach ($result->result_array() as $row) {
+			$data[] = array(
 				'transaction_id' => $row['transaction_id'],
 				'account_number' => $row['account_number'],
-				'amount' => $row['amount'],
-				't_date' => $row['t_date']
-				);
+				'amount' => $row['amount']
+			);
+		}
 
-			}
-
-
-    		return $data;
-	
-	
-	
-	
-	}
-	
+    	return $data;
+    }
 
 
 
