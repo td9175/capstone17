1986b58ca2f94bf4b4e4c036f0f5147ce4107766
rwfwcb <rwfwcb@mail.missouri.edu>
2017-03-14 20:48:06 -0500

    Fixing yelp api errors

--- ci/application/controllers/HealthServices.php
+++ ci/application/controllers/HealthServices.php
@@ -17,6 +17,9 @@ class HealthServices extends CI_Controller {
 
   public function health_services_search(){
 
+		// Yelp Fusion Search API endpoint
+		$endpoint = "https://api.yelp.com/v3/businesses/search";
+
 		// Authentication stuff for Yelp Fusion API
 		$clientId = $this->config->item('clientId');
 		$clientSecret = $this->config->item('clientSecret');
@@ -27,35 +30,39 @@ class HealthServices extends CI_Controller {
 		// Constant CATEGORIES. All searches must be contained within the health category
 		define("CATEGORIES", "health");
 
-		// Parameters for the business search API
-		$endpoint = "https://api.yelp.com/v3/businesses/search";
-		$searchTerm = $this->input->post('term'); // Optional
-    $location = $this->input->post('location'); // Required if either latitude or longitude is not provided. Specifies the combination of "address, neighborhood, city, state or zip, optional country" to be used when searching for businesses.
-		$radius = $this->input->post('radius'); // Optional search radius in meters, max value is 40000 meters (25 miles)
-    // $latitude;
-    // $longitude;
-    // $locale;
-    // $limit;
-    // $offset;
-    // $sort_by;
-    // $price;
-    // $open_now;
-    // $open_at;
-    // $attributes;
+		// Parameters array for the business search API
+		$params = array(
+			'term' => urlencode($this->input->post('term')), // Optional
+			'location' => urlencode($this->input->post('location')), // Required if either latitude or longitude is not provided. Specifies the combination of "address, neighborhood, city, state or zip, optional country" to be used when searching for businesses.
+			'radius' => urlencode($this->input->post('radius')), // Optional search radius in meters, max value is 40000 meters (25 miles)
+			'categories' => 'CATEGORIES'
+			// $latitude;
+			// $longitude;
+			// $locale;
+			// $limit;
+			// $offset;
+			// $sort_by;
+			// $price;
+			// $open_now;
+			// $open_at;
+			// $attributes;
+	);
+
+		// URL-ify the data for the POST
+		foreach($params as $key=>$value){
+			$postString .= $key.'='.$value.'&';
+		}
+		rtrim($fields_string, '&');
 
 		// Initialize curl and set options
-    $curl = curl_init($endpoint);
+    $curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $endpoint);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_POST, true);
 		curl_setopt($curl, CURLOPT_HEADER, true);
-    curl_setopt($curl, CURLOPT_HEADER,'Content-Type: application/x-www-form-urlencoded');
+		curl_setopt($curl, CURLOPT_HEADER,'Content-Type: application/x-www-form-urlencoded');
 		curl_setopt($curl, CURLOPT_HEADER,'Authorization: Bearer ' . $accessToken);
-
-		// Build the post string and sanitize the user input
-		$postData = "term=" . urlencode($searchTerm) . "&location=" . urlencode($location) . "&radius=" . urlencode($radius);
-
-		// Set the post fields option for curl
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $postData);
+		curl_setopt($curl, CURLOPT_POST, count($params));
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $postString);
 
 		// Execute the curl request on Yelp Fusion API
     $json_response = curl_exec($curl);
@@ -64,9 +71,9 @@ class HealthServices extends CI_Controller {
     $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
     // Evaluate for success response
-    if ($status != 200) {
-      throw new Exception("Error: call to URL $endpoint failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl) . "\n");
-    }
+    // if ($status != 200) {
+    //   throw new Exception("Error: call to URL $endpoint failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl) . "\n");
+    // }
 
 		// Close curl
     curl_close($curl);
