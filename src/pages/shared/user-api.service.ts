ba7816da7093e781034de1c2639de74ab9eccd7f
Kenny Clark <kenny.birmanov.clark@gmail.com>
2017-03-17 00:03:47 -0500

    Working api call to sami user/hsa

--- src/pages/shared/user-api.service.ts
+++ src/pages/shared/user-api.service.ts
@@ -4,26 +4,29 @@ import { Http, Response } from '@angular/http';
 @Injectable()
 export class UserApi {
 
-    // private baseUrl = 'http://capstone.td9175.com';
-    private baseUrl = 'https://capstone17-umbhealth-i2.firebaseio.com';
+    private baseUrl = 'http://capstone.td9175.com';
     
-    // userdata: any; 
+    items: any
+    //userid: any
 
     constructor(private http: Http) { }
-    /*
+    
+    // much much better way to do this.
     getUserData(){
         return new Promise(resolve => {
             this.http.get(`${this.baseUrl}/ci/index.php/rest/hsa/id/19.json`)
                 .subscribe(res => resolve(res.json()));
         });
-    */
-        
+    }
 
+    // this is for testing purposes, working json file stored on firebase.
+    // private baseUrl = 'https://capstone17-umbhealth-i2.firebaseio.com';
+    /*
     getUserData(){
     return new Promise(resolve => {
-        this.http.get(`${this.baseUrl}/0`)
+        this.http.get(`${this.baseUrl}/0.json`)
             .subscribe(res => resolve(res.json()));
-    });
-    
+        });
     }
+    */
 } //end export class
