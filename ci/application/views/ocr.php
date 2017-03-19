93d7c787d22edf48a1c5e439aaa1d190999b86b8
skh6yf <skh6yf@mail.missouri.edu>
2017-03-19 18:51:50 -0500

    file upload tests

--- ci/application/views/ocr.php
+++ ci/application/views/ocr.php
@@ -1,48 +1,20 @@
 <html>
 <head>
-
-  <title>Registration UMB Health Spending App</title>
-
-  <!-- Load jQuery -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
-  <script>
-  $(document).ready(function(){
-      $("#submit_image").click(function(){
-
-        $("#submit_image").text = "Loading";
-
-        var image= $('#image').val();
-     	console.log("Image");
-     	console.log(image);
-        var url = "https://capstone.td9175.com/ci/index.php/Rest/upload";
-        console.log(url);
-
-          $.post(url,
-          {
-            image: $('#image').val()
-            
-          },
-          function(data, status){
-              $("#response").text = "Data: " + data + "\nStatus: " + status;
-          });
-
-      });
-  });
-  </script>
-
+<title>Upload Form</title>
 </head>
-
 <body>
 
-  <h1>OCR Testing</h1>
+<?php echo $error;?>
 
-  <input type='file' id='image' name='image'><br><br>
-  
-  <button id='submit_image'>Upload</button><br><br>
+<?php echo form_open_multipart('upload/do_upload');?>
 
-  <p id='response'></p>
+<input type="file" name="userfile" size="20" />
+
+<br /><br />
+
+<input type="submit" value="upload" />
+
+</form>
 
 </body>
-
 </html>
