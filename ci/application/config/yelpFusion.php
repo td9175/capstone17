5bc7b13014ade27670b00a044fbe40d82dc834c2
rwfwcb <rwfwcb@mail.missouri.edu>
2017-03-18 19:04:23 -0500

    Fixed Yelp API, made some updates

--- ci/application/config/yelpFusion.php
+++ ci/application/config/yelpFusion.php
@@ -3,5 +3,12 @@
 $config['clientId'] = "E_XYQCIIe8GmzFeMCk1V8w";
 $config['clientSecret'] = "KbuR0Ndg15O6ocAfftVnlfkyrr7InjAwlxXnIzpMbCZPX4Ta7hnW2xKAFFRqVCHI";
 $config['grantType'] = "client_credentials";
+$config['apiHost'] = "https://api.yelp.com";
+$config['tokenPath'] = "/oauth2/token";
+$config['searchPath'] = "/v3/businesses/search";
+$config['businessPath'] = "/v3/businesses/";
+$config['categories'] = "health";
+
+
 
 ?>
--- ci/application/controllers/YelpHealthServiceRequest.php
+++ ci/application/controllers/YelpHealthServiceRequest.php
@@ -1,3 +1,11 @@
+<!--
+
+@Author: Robert Fink
+12bit - UMB Bank Health Spending App
+
+-->
+
+
 <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
@@ -7,7 +15,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  *
  * This program demonstrates the capability of the Yelp Fusion API
  * by using the Business Search API to query for businesses by a
- * search term, location, and radius. Then using the Yelp Business API to query additional
+ * search term, location, and radius. Then using the Yelp Business and Review API's to query additional
  * information about the results from the search query.
  *
  * Please refer to http://www.yelp.com/developers/v3/documentation
@@ -19,54 +27,39 @@ class YelpHealthServiceRequest extends CI_Controller {
 	{
         parent::__construct();
         $this->config->load('yelpFusion');
-
-				// API secret stuff
-	      $clientId = $this->config->item("clientId");
-	      $clientSecret = $this->config->item('clientSecret');
-	      $grantType = $this->config->item('grantType');
-
-				// Yelp Fusion API constants
-				$tokenPath = "/oauth2/token";
-	      $apiHost = "https://api.yelp.com";
-	      $searchPath = "/v3/businesses/search";
-	      $categories = "health";
-	      $searchLimit = 10;
-
-				// Get user input
-	      $term = $this->input->post('term');
-	      $location = $this->input->post('location');
-
-        // $businessPath = "/v3/businesses/";  // Business ID will come after slash.
-        // Defaults for our simple example.
-        // $defaultCategory = "health";
-        // $defaultTerm = "health";
-        // $defaultLocation = "65201";
   }
 
 
   /**
-   * Given a bearer token, send a GET request to the API.
+   * Send a GET request to the Yelp API
    *
    * @return   OAuth bearer token, obtained using clientId and clientSecret.
    */
    function obtain_bearer_token() {
 
+     // Yelp Fusion API secret stuff
+     $clientId = $this->config->item("clientId");
+     $clientSecret = $this->config->item('clientSecret');
+     $grantType = $this->config->item('grantType');
 
+     // Yelp Fusion API constants
+     $apiHost = $this->config->item('apiHost');
+     $tokenPath = $this->config->item('tokenPath');
 
       try {
-          # Using the built-in cURL library for easiest installation.
-          # Extension library HttpRequest would also work here.
-          $curl = curl_init();
 
+					// Initialize curl and make sure it worked
+          $curl = curl_init();
           if (FALSE === $curl){
             throw new Exception('Failed to initialize');
           }
 
-              // Buid the post fields string for authentication
-              $postfields = "client_id=" . $this->clientId . "&client_secret=" . $this->clientSecret . "&grant_type=" . $this->grantType;
+              // Build the post fields string for authentication
+              $postfields = "client_id=" . $clientId . "&client_secret=" . $clientSecret . "&grant_type=" . $grantType;
 
+							// Set an array of curl options
               curl_setopt_array($curl, array(
-                CURLOPT_URL => $this->apiHost . $this->tokenPath,
+                CURLOPT_URL => $apiHost . $tokenPath,
                 CURLOPT_RETURNTRANSFER => true,  // Capture response.
                 CURLOPT_ENCODING => "",
                 CURLOPT_MAXREDIRS => 10,
@@ -80,18 +73,19 @@ class YelpHealthServiceRequest extends CI_Controller {
                 ),
               ));
 
+					// Execute a curl request and make sure curl did not fail
           $response = curl_exec($curl);
-
           if (FALSE === $response){
             throw new Exception(curl_error($curl), curl_errno($curl));
           }
 
+					// Make sure status is 200 Success
           $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
           if (200 != $http_status){
             throw new Exception($response, $http_status);
           }
 
+					// Close curl
           curl_close($curl);
 
       } catch(Exception $e) {
@@ -105,6 +99,7 @@ class YelpHealthServiceRequest extends CI_Controller {
       return $bearer_token;
   }
 
+
   /**
    * Makes a request to the Yelp API and returns the response
    *
@@ -115,20 +110,24 @@ class YelpHealthServiceRequest extends CI_Controller {
    * @return   The JSON response from the request
    */
    function request($bearer_token, $host, $path, $url_params = array()) {
-      // Send Yelp API Call
-      try {
-          $curl = curl_init();
 
+      // Try to send a Yelp API Call
+      try {
+
+					// Initialize curl and make sure it worked
+          $curl = curl_init();
           if (FALSE === $curl){
             throw new Exception('Failed to initialize');
           }
 
+					// Generate URL-encoded query string
           $url = $host . $path . "?" . http_build_query($url_params);
 
+					// Set an array of curl options
           curl_setopt_array($curl, array(
               CURLOPT_URL => $url,
               CURLOPT_RETURNTRANSFER => true,  // Capture response.
-              CURLOPT_ENCODING => "",  // Accept gzip/deflate/whatever.
+              CURLOPT_ENCODING => "",
               CURLOPT_MAXREDIRS => 10,
               CURLOPT_TIMEOUT => 30,
               CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
@@ -139,16 +138,19 @@ class YelpHealthServiceRequest extends CI_Controller {
               ),
           ));
 
+					// Execute a curl request and make sure curl did not fail
           $response = curl_exec($curl);
           if (FALSE === $response){
             throw new Exception(curl_error($curl), curl_errno($curl));
           }
 
+					// Make sure status is 200 Success
           $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
           if (200 != $http_status){
             throw new Exception($response, $http_status);
           }
 
+					// Close curl
           curl_close($curl);
 
       } catch(Exception $e) {
@@ -158,82 +160,97 @@ class YelpHealthServiceRequest extends CI_Controller {
       return $response;
   }
 
+
   /**
    * Query the Search API by a search term and location
    *
    * @param    $bearer_token   API bearer token from obtain_bearer_token
+	 * @param    $categories  The category to filter by
    * @param    $term        The search term passed to the API
    * @param    $location    The search location passed to the API
-   * @param    $categories  The category to filter by
+	 * @param    $radius      The radius to search within the location
+	 * @param    $searchLimit The number to limit search results by
    * @return   The JSON response from the request
    */
-   function search($bearer_token, $term, $location, $categories, $searchLimit) {
+   function search($bearer_token, $categories, $term, $location, $radius, $searchLimit) {
 
+     // Yelp Fusion API constants
+     $apiHost = $this->config->item('apiHost');
+     $searchPath = $this->config->item('searchPath');
 
      // Build the paramater array
      $url_params = array();
-      $url_params['term'] = $term;
+		 	$url_params['categories'] = $categories;
+    	$url_params['term'] = $term;
       $url_params['location'] = $location;
-      $url_params['categories'] = $categories;
-      $url_params['limit'] = $searchLimit;
+			$url_params['radius'] = $radius;
+			$url_params['limit'] = $searchLimit;
 
       // Call the API request method
-      $response = $this->request($bearer_token, $this->apiHost, $this->searchPath, $url_params);
+      $response = $this->request($bearer_token, $apiHost, $searchPath, $url_params);
 
       return $response;
-
   }
-
-
-  // /**
-  //  * Query the Business API by business_id
-  //  *
-  //  * @param    $bearer_token   API bearer token from obtain_bearer_token
-  //  * @param    $business_id    The ID of the business to query
-  //  * @return   The JSON response from the request
-  //  */
-  // function get_business($bearer_token, $business_id) {
-  //     $businessPath = $businessPath . urlencode($business_id);
-  //
-  //     return request($bearer_token, $GLOBALS['apiHost'], $businessPath);
-  // }
 
 
   /**
    * Queries the API by the input values from the user
    *
+	 * @param    $categories  The category to filter by (constant HEALTH)
    * @param    $term        The search term to query
    * @param    $location    The location of the business to query
-   * @param    $categories  The category to filter by
+	 * @param    $radius      The radius to search within the location
+	 * @param    $searchLimit The number to limit search results by
    */
    function query_api() {
+     // Constants
+     $categories = $this->config->item('categories');
+
+     // Get user input
+     $term = $this->input->post('term');
+     $location = $this->input->post('location');
+		 $radius = $this->input->post('radius');
+		 $searchLimit = $this->input->post('searchLimit');
 
      // Get the bearer token
      $bearer_token = $this->obtain_bearer_token();
 
      // Send a request to the search method
-     $response = json_decode($this->search($bearer_token, $this->term, $this->location, $this->categories, $this->searchLimit));
+     $response = json_decode($this->search($bearer_token, $categories, $term, $location, $radius, $searchLimit));
 
      var_dump($response);
-
-
-      //  print "$response\n";
-      // $business_id = $response->businesses[0]->id;
-      //
-      // print sprintf(
-      //     "%d businesses found, querying business info for the top result \"%s\"\n\n",
-      //     count($response->businesses),
-      //     $business_id
-      // );
-      //
-      // $response = get_business($bearer_token, $business_id);
-      //
-      // print sprintf("Result for business \"%s\" found:\n", $business_id);
-      // $pretty_response = json_encode(json_decode($response), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-      // print "$pretty_response\n";
   }
 
 }
 
+
+// /**
+//  * Query the Business API by business_id
+//  *
+//  * @param    $bearer_token   API bearer token from obtain_bearer_token
+//  * @param    $business_id    The ID of the business to query
+//  * @return   The JSON response from the request
+//  */
+// function get_business($bearer_token, $business_id) {
+//     $businessPath = $businessPath . urlencode($business_id);
+//
+//     return request($bearer_token, $GLOBALS['apiHost'], $businessPath);
+// }
+
+
+//  print "$response\n";
+// $business_id = $response->businesses[0]->id;
+//
+// print sprintf(
+//     "%d businesses found, querying business info for the top result \"%s\"\n\n",
+//     count($response->businesses),
+//     $business_id
+// );
+//
+// $response = get_business($bearer_token, $business_id);
+//
+// print sprintf("Result for business \"%s\" found:\n", $business_id);
+// $pretty_response = json_encode(json_decode($response), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+// print "$pretty_response\n";
 
 ?>
