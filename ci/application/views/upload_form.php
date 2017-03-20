1458ea63d0d7c567e8cea4f30f1493799399e1cd
skh6yf <skh6yf@mail.missouri.edu>
2017-03-20 18:15:15 -0500

    form helper

--- ci/application/views/upload_form.php
+++ ci/application/views/upload_form.php
@@ -1,5 +1,6 @@
 <?php 
-	
+	require('system/helpers/form_helper.php');
+
 	echo form_open('Drugs/search_for_drug', array('method'=>'get')); ?>
 	<label for='searchQuery'>Enter a drug to search for: </label>
 	<input type='text' name='searchQuery'>
