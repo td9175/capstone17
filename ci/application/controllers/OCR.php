211ca99aba19f465786347070b02c330927f42cd
skh6yf <skh6yf@mail.missouri.edu>
2017-03-20 17:05:18 -0500

    updating OCR

--- ci/application/controllers/OCR.php
+++ ci/application/controllers/OCR.php
@@ -4,63 +4,7 @@ require_once(APPPATH.'HTTP_Request2-2.3.0/HTTP/Request2.php');
 
 class Ocr extends CI_Controller {
 	    
-	    //needs to read in an image file
-	    //upload that image file to server
-	    //pull that image file from server and send to API
-	    //send back JSON data to front end
-function upload_post() {
-		$target_dir = "uploads/";
-		
-		
-		$image = $this->post('image');
-		
-		echo "in upload_image";
-		//fileToUpload is post variable from form
-		
-		$target_file = $target_dir . basename($_FILES[$image]["name"]);
-		$uploadOk = 1;
-		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-		// Check if image file is a actual image or fake image
-		if(isset($_POST["submit"])) {
-			$check = getimagesize($_FILES[$image]["tmp_name"]);
-			if($check !== false) {
-				echo "File is an image - " . $check["mime"] . ".";
-				$uploadOk = 1;
-			} else {
-				echo "File is not an image.";
-				$uploadOk = 0;
-			}
-		}
-		// Check if file already exists
-		if (file_exists($target_file)) {
-			echo "Sorry, file already exists.";
-			$uploadOk = 0;
-		}
-		// Check file size
-		if ($_FILES[$image]["size"] > 500000) {
-			echo "Sorry, your file is too large.";
-			$uploadOk = 0;
-		}
-		// Allow certain file formats
-		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-		&& $imageFileType != "gif" ) {
-			echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-			$uploadOk = 0;
-		}
-		// Check if $uploadOk is set to 0 by an error
-		if ($uploadOk == 0) {
-			echo "Sorry, your file was not uploaded.";
-		// if everything is ok, try to upload file
-		} else {
-			if (move_uploaded_file($_FILES[$image]["tmp_name"], $target_file)) {
-				echo "The file ". basename( $_FILES[$image]["name"]). " has been uploaded.";
-			} else {
-				echo "Sorry, there was an error uploading your file.";
-			}
-		}
 
-
-}
 	    
 function ocr_request() {
 			$request = new Http_Request2('https://westus.api.cognitive.microsoft.com/vision/v1.0/ocr');
@@ -96,7 +40,7 @@ function ocr_request() {
 			$newurl = "'}";
 			echo "url: "  . $newurl;
 			echo "<br><Br>";
-			$request->setBody($image);
+			$request->setBody("{'url':'http://i1008.photobucket.com/albums/af202/CompSyn/Walmart_QS_zpsw3uk1oeh.png'}");
 
 			try {
 				$response = $request->send();
