24cafd263167937c99c27c788d706e7bfaa1cc4d
skh6yf <skh6yf@mail.missouri.edu>
2017-03-19 19:41:33 -0500

    redoing upload

--- ci/application/controllers/Rest.php
+++ ci/application/controllers/Rest.php
@@ -128,59 +128,7 @@ require('application/libraries/REST_Controller.php');
 	  			}
       }
       
-function upload_post() {
-		$target_dir = "uploads/";
-		
-		
-		$image = $this->post('image');
-		
-		echo "in upload_image";
-		//fileToUpload is post variable from form
-		
-		$target_file = $target_dir . basename($_FILES[$image]["name"]);
-		$uploadOk = 1;
-		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-		// Check if image file is a actual image or fake image
-		if(isset($_POST["submit"])) {
-			$check = getimagesize($_FILES[$image]["tmp_name"]);
-			if($check !== false) {
-				echo "File is an image - " . $check["mime"] . ".";
-				$uploadOk = 1;
-			} else {
-				echo "File is not an image.";
-				$uploadOk = 0;
-			}
-		}
-		// Check if file already exists
-		if (file_exists($target_file)) {
-			echo "Sorry, file already exists.";
-			$uploadOk = 0;
-		}
-		// Check file size
-		if ($_FILES[$image]["size"] > 500000) {
-			echo "Sorry, your file is too large.";
-			$uploadOk = 0;
-		}
-		// Allow certain file formats
-		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-		&& $imageFileType != "gif" ) {
-			echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-			$uploadOk = 0;
-		}
-		// Check if $uploadOk is set to 0 by an error
-		if ($uploadOk == 0) {
-			echo "Sorry, your file was not uploaded.";
-		// if everything is ok, try to upload file
-		} else {
-			if (move_uploaded_file($_FILES[$image]["tmp_name"], $target_file)) {
-				echo "The file ". basename( $_FILES[$image]["name"]). " has been uploaded.";
-			} else {
-				echo "Sorry, there was an error uploading your file.";
-			}
-		}
 
-
-}
 	
       
       
@@ -248,29 +196,25 @@ function upload_post() {
       
       }
       
-      function do_upload()
+      function upload_post()
 	{
-		$this->load->helper(array('form', 'url'));
-		$config['upload_path'] = './uploads/';
-		$config['allowed_types'] = 'gif|jpg|png';
-		$config['max_size']	= '100';
-		$config['max_width']  = '1024';
-		$config['max_height']  = '768';
+		echo "in upload_post()";
+        // Load the model
+        //$this->load->model('UserAccountModel');
 
-		$this->load->library('upload', $config);
+				// Get user information for registration
+        $image = $this->post('image');
+		
 
-		if ( ! $this->upload->do_upload())
-		{
-			$error = array('error' => $this->upload->display_errors());
 
-			$this->load->view('ocr', $error);
-		}
-		else
-		{
-			$data = array('upload_data' => $this->upload->data());
-			echo "success";
-			//$this->load->view('registration', $data);
-		}
+        // If registration_response has data respond with data and success, or 404
+        if($image){
+        		echo "success";
+  				$this->response($image, 200); // 200 Success
+  			} else {
+  				echo "fail";
+  				$this->response(NULL, 404); // 404 Not found
+  			}
 	}
 
 	
