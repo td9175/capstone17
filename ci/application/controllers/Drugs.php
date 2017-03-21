15effc49b0bc96bbeba6ed2888593769398a9d28
skh6yf <skh6yf@mail.missouri.edu>
2017-03-20 20:23:21 -0500

    s

--- ci/application/controllers/Drugs.php
+++ ci/application/controllers/Drugs.php
@@ -107,7 +107,7 @@ class Drugs extends CI_Controller {
          $config['max_size']      = 100; 
          $config['max_width']     = 1024; 
          $config['max_height']    = 768;  
-         $this->load->library('upload', $config);
+         //$this->load->library('upload', $config);
          
          echo "Uplaod path: ";
          echo $config['upload_path'];
