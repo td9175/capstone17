15f89add030e37e6fbddf1466e6689d9d457fa8e
skh6yf <skh6yf@mail.missouri.edu>
2017-03-20 19:47:29 -0500

    changing method to get instead of post

--- ci/application/views/upload_form.php
+++ ci/application/views/upload_form.php
@@ -3,7 +3,7 @@
 	
 
 
-	echo form_open('Drugs/do_upload', array('method'=>'post')); ?>
+	echo form_open('Drugs/do_upload', array('method'=>'get')); ?>
 	<label for='userfile'>Upload File:</label>
 	<input type='file' name='userfile'>
 	<input type='submit' name='submit' value='Upload'>
