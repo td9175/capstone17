51bc11715e7364e0f095b7228bb2f4965a6555c6
skh6yf <skh6yf@mail.missouri.edu>
2017-03-20 19:42:44 -0500

    moving do_upload function to Drug.php
    just for testing purposes

--- ci/application/controllers/Drugs.php
+++ ci/application/controllers/Drugs.php
@@ -100,5 +100,23 @@ class Drugs extends CI_Controller {
       // Echo the response for RESTful API calls
       echo $response;
     }
+          public function do_upload() { 
+         $config['upload_path']   = './uploads/'; 
+         $config['allowed_types'] = 'gif|jpg|png'; 
+         $config['max_size']      = 100; 
+         $config['max_width']     = 1024; 
+         $config['max_height']    = 768;  
+         $this->load->library('upload', $config);
+			
+         if ( ! $this->upload->do_upload('userfile')) {
+            $error = array('error' => $this->upload->display_errors()); 
+            $this->load->view('upload_form', $error); 
+         }
+			
+         else { 
+            $data = array('upload_data' => $this->upload->data()); 
+            $this->load->view('upload_success', $data); 
+         } 
+      } 
 
 }
--- ci/application/views/upload_form.php
+++ ci/application/views/upload_form.php
@@ -3,8 +3,8 @@
 	
 
 
-	echo form_open('Upload/do_upload', array('method'=>'post')); ?>
-	<label for='searchQuery'>Upload File:</label>
+	echo form_open('Drugs/do_upload', array('method'=>'post')); ?>
+	<label for='userfile'>Upload File:</label>
 	<input type='file' name='userfile'>
 	<input type='submit' name='submit' value='Upload'>
 	</form>
