d349349c135a81d22c5e063cf0678c993e07332b
skh6yf <skh6yf@mail.missouri.edu>
2017-03-20 20:01:14 -0500

    oops

--- ci/application/views/upload_form.php
+++ ci/application/views/upload_form.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 <?php echo $error;?> <!-- Error Message will show up here -->
-<?php echo form_open_multipart('UploadFile/do_upload');?>
+<?php echo form_open_multipart('Drugs/do_upload');?>
 <?php echo "<input type='file' name='userfile' size='20' />"; ?>
 <?php echo "<input type='submit' name='submit' value='upload' /> ";?>
 <?php echo "</form>"?>
