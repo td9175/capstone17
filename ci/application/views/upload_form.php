33dbf94f40f38c00f3d4e477b3a6bc10a28c8b15
skh6yf <skh6yf@mail.missouri.edu>
2017-04-04 14:46:34 -0500

    file upload form_open test

--- ci/application/views/upload_form.php
+++ ci/application/views/upload_form.php
@@ -4,10 +4,11 @@
 <title>Upload Form</title>
 </head>
 <body>
-<?php echo $error;?> <!-- Error Message will show up here -->
-<?php echo form_open_multipart('Drugs/do_upload');?>
-<?php echo "<input type='file' name='userfile' size='20' />"; ?>
-<?php echo "<input type='submit' name='submit' value='upload' /> ";?>
-<?php echo "</form>"?>
+<?php echo $error;
+echo form_open('Drugs/do_upload', array('method'=>'post')); ?>
+	<label for='searchQuery'>Upload File:</label>
+	<input type='file' name='userfile'>
+	<input type='submit' name='submit' value='Upload'>
+	</form> 
 </body>
 </html>
