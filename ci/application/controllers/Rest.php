d0c0122b7129199fb38fcf2e1d143fe26e439d4b
skh6yf <skh6yf@mail.missouri.edu>
2017-04-04 16:21:24 -0500

    d

--- ci/application/controllers/Rest.php
+++ ci/application/controllers/Rest.php
@@ -199,10 +199,9 @@ require('application/libraries/REST_Controller.php');
     
     $data['upload_data'] = '';
     
-		//load the view/upload.php with $data
-		$this->load->view('upload', $data);
+
     
-		$this->load->view('upload_form', $error);
+		$this->load->view('upload_form', $data);
 
 
 	}
