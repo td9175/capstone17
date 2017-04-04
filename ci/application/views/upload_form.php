b1f68aea6bc3be4b1f25bf5dba22df8265edc3cf
skh6yf <skh6yf@mail.missouri.edu>
2017-04-04 16:27:29 -0500

    DF

--- ci/application/views/upload_form.php
+++ ci/application/views/upload_form.php
@@ -5,7 +5,16 @@
 </head>
 <body>
 <code><?php echo $msg;?></code>
-<?php 
+
+
+<code>
+<?php if($upload_data != ''):?>
+<?php var_dump($upload_data);?>
+
+</code>
+<img scr="<?php echo $upload_data['full_path'];?>">
+<?php endif;
+
 echo form_open('Drugs/do_upload'); ?>
 	<label for='userfile'>Upload File:</label>
 	<input type='file' name='userfile'>
