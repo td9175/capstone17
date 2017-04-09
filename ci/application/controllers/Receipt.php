55ba7d30ed8ebecc3eba458a77f79a3b974425d0
skh6yf <skh6yf@mail.missouri.edu>
2017-04-09 16:29:56 -0500

    s

--- ci/application/controllers/Receipt.php
+++ ci/application/controllers/Receipt.php
@@ -6,52 +6,57 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		//function to insert receipt into DB
 		//function to insert OCR info into DB
 		function upload_it() {
-		//load the helper
-		$this->load->helper('form');
-		$this->load->model('ReceiptModel');
+			//load the helper
+			$this->load->helper('form');
+			$this->load->model('ReceiptModel');
 
-		//need to create folder for each user, path will be unique to the user
-		$email = $_SESSION['email'];
-		echo "Email: ". $email; 
-		$date = date('d-m-y');
-		$time = time();
+			//need to create folder for each user, path will be unique to the user
+			$email = $_SESSION['email'];
+			echo "Email: ". $email; 
+			$date = date('d-m-y');
+			echo "<br>Date:" . $date;
+			
+			$time = time();
+			echo "<br>Time:" . $time;
 		
-		$config['upload_path'] = '/var/www/html/ci/application/receipts/';
-		$config['upload_path'] .= $email;
+			$config['upload_path'] = '/var/www/html/ci/application/receipts/';
+			$config['upload_path'] .= $email;
 		
-		//need to change filename to be unique to the user
-		$config['file_name'] = $date .':'.$time;
+			//need to change filename to be unique to the user
+			$config['file_name'] = $date .':'.$time;
 
-		echo "Upload path: " . $config['upload_path'];
+			echo "Upload path: " . $config['upload_path'];
+			echo "<br><br>";
+			echo "File name: " . $config['file_name'];
 
-    // set the filter image types
-		$config['allowed_types'] = 'gif|jpg|png';
+		// set the filter image types
+			$config['allowed_types'] = 'gif|jpg|png';
 
-		//load the upload library
-		$this->load->library('upload', $config);
+			//load the upload library
+			$this->load->library('upload', $config);
 
-    	$this->upload->initialize($config);
-    	$this->upload->set_allowed_types('*');
+			$this->upload->initialize($config);
+			$this->upload->set_allowed_types('*');
 
-		$data['upload_data'] = '';
-		$path = '';
+			$data['upload_data'] = '';
+			$path = '';
 
-		//if not successful, set the error message
-		if (!$this->upload->do_upload('userfile')) {
-			$data = array('msg' => $this->upload->display_errors());
+			//if not successful, set the error message
+			if (!$this->upload->do_upload('userfile')) {
+				$data = array('msg' => $this->upload->display_errors());
 
-		} else { //else, set the success message
-			$data = array('msg' => "Upload success!");
+			} else { //else, set the success message
+				$data = array('msg' => "Upload success!");
 
-      		$data['upload_data'] = $this->upload->data('full_path');
+				$data['upload_data'] = $this->upload->data('full_path');
 
-      		//insert into DB
-      		$path = $this->ReceiptModel->receiptData_post($data['upload_data'], $this->get('id'));
+				//insert into DB
+				$path = $this->ReceiptModel->receiptData_post($data['upload_data'], $this->get('id'));
 
 
-		}
+			}
 
-		// load the view/upload.php
+			// load the view/upload.php
 		$this->load->view('upload_form', $data);
 
 	}
