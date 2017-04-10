a7202979d068b358bd47679b5156424e173287c0
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-10 17:51:46 -0500

    Added gather_fsa_post(), gather_hsa_post()

--- ci/application/controllers/HealthAccount.php
+++ ci/application/controllers/HealthAccount.php
@@ -4,16 +4,32 @@ require('application/libraries/REST_Controller.php');
 
 class HealthAccount extends REST_Controller {
 
-  function hsa_get() {
+  // function hsa_get() {
+  //   $this->load->model('HealthAccountModel');
+  //
+  //   if(!$this->get('email')){
+  //     $this->response(NULL, 400);
+  //   }
+  //
+  //   $decoded_email = urldecode($this->get('email'));
+  //
+  //   $user = $this->HealthAccountModel->get_hsa_info($decoded_email);
+  //
+  //   if($user) {
+  //     $this->response($user, 200); // 200 being the HTTP response code
+  //   } else {
+  //     $this->response(NULL, 404);
+  //   }
+  // }
+
+  function gather_hsa_post() {
     $this->load->model('HealthAccountModel');
 
     if(!$this->get('email')){
       $this->response(NULL, 400);
     }
 
-    $decoded_email = urldecode($this->get('email'));
-
-    $user = $this->HealthAccountModel->get_hsa_info($decoded_email);
+    $user = $this->HealthAccountModel->get_hsa_info($this->get('email'));
 
     if($user) {
       $this->response($user, 200); // 200 being the HTTP response code
@@ -23,15 +39,32 @@ class HealthAccount extends REST_Controller {
   }
 
 
-  function fsa_get() {
+  // function fsa_get() {
+  //   $this->load->model('HealthAccountModel');
+  //
+  //   if(!$this->get('email'))
+  //   {
+  //     $this->response(NULL, 400);
+  //   }
+  //
+  //   $decoded_email = urldecode($this->get('email'));
+  //
+  //   $user = $this->HealthAccountModel->get_fsa_info($decoded_email);
+  //
+  //   if($user){
+  //     $this->response($user, 200); // 200 being the HTTP response code
+  //   } else {
+  //     $this->response(NULL, 404);
+  //   }
+  // }
+
+  function gather_fsa_post() {
     $this->load->model('HealthAccountModel');
 
     if(!$this->get('email'))
     {
       $this->response(NULL, 400);
     }
-
-    $decoded_email = urldecode($this->get('email'));
 
     $user = $this->HealthAccountModel->get_fsa_info($decoded_email);
 
