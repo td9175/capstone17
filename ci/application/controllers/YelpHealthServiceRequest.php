37a7b1eca502fbf512c3b4a810520dc1aee4e5ed
td9175 <td9175@gmail.com>
2017-04-09 19:07:40 -0500

    Add header line for CORS problem

--- ci/application/controllers/YelpHealthServiceRequest.php
+++ ci/application/controllers/YelpHealthServiceRequest.php
@@ -7,6 +7,7 @@
 ************************************************************************/
 
 defined('BASEPATH') OR exit('No direct script access allowed');
+header("Access-Control-Allow-Origin: *");
 
 /************************************************************************
  * Yelp Fusion API code.
