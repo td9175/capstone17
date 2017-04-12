cb498363efb6a0f69eb320afaf7c85a49be1e513
MichaelRTompkins <michaelrtompkins@gmail.com>
2017-04-12 00:11:53 -0500

    Product Compare Kinda Works

--- src/pages/product-detail/product-detail.ts
+++ src/pages/product-detail/product-detail.ts
@@ -1,3 +1,4 @@
+import { merge } from 'rxjs/operator/merge';
 import { ProductsPage } from './../products/products';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
@@ -15,21 +16,26 @@ import { UserApi } from './../shared/user-api.service';
 })
 export class ProductDetail {
 
-  public comparisonJson: any;
+  public priceComparisonJson: any;
+  public storeComparisonJson: any
+  public productToGetDetails: any;
+  public compareArray: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public userApi: UserApi) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProductDetailPage');
     console.log(this.userApi.drugToGetDetails);
+    this.productToGetDetails = this.userApi.drugToGetDetails;
     this.loadPrices();
+    this.loadStores();
   }
 
   loadPrices(){
     this.userApi.getProductPrices().subscribe(
       result => {
-        this.comparisonJson=result.data;
-        console.log("Success : "+this.comparisonJson);
+        this.priceComparisonJson=result.data.price_detail.price;
+        console.log("Success : "+this.priceComparisonJson);
       },
       err =>{
         console.error("Error : "+err);
@@ -38,6 +44,25 @@ export class ProductDetail {
         console.log('getData completed');
       }
     );
+  }
+
+  loadStores(){
+    this.userApi.getProductPrices().subscribe(
+      result => {
+        this.storeComparisonJson=result.data.price_detail.pharmacy;
+        console.log("Success : "+this.storeComparisonJson);
+      },
+      err =>{
+        console.error("Error : "+err);
+      } ,
+      () => {
+        console.log('getData completed');
+      }
+    );
+  }
+
+  loadArray(){
+    
   }
 
 }
