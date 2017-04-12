8bec55416eba54dcaebd1b77249e08131c0f3a62
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-11 22:39:03 -0500

    Troubleshooting

--- ci/application/controllers/AccountTransaction.php
+++ ci/application/controllers/AccountTransaction.php
@@ -14,8 +14,11 @@ require('application/libraries/REST_Controller.php');
 			if(!$this->get('email')) {
 				$this->response(NULL, 400);
 			}
+			$email = $this->get('email');
+			echo "$email \n";
 			// URL decode the email
 			$decoded_email = json_decode($this->get('email'));
+			echo "$decoded_email \n";
 			// Call the transaction_history function in the model
 			$response = $this->AccountTransactionModel->get_transaction_history($decoded_email);
 			// Verify there is something, and respond with the JSON
