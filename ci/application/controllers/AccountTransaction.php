7722e0c461e7d7d2103575d7534f4709d0b8ffe3
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-11 22:38:03 -0500

    Fixed account transaction_history()

--- ci/application/controllers/AccountTransaction.php
+++ ci/application/controllers/AccountTransaction.php
@@ -14,11 +14,8 @@ require('application/libraries/REST_Controller.php');
 			if(!$this->get('email')) {
 				$this->response(NULL, 400);
 			}
-			$email = $this->get('email');
-			echo "$email \n";
 			// URL decode the email
-			$decoded_email = $this->get('email');
-			echo "$decoded_email \n";
+			$decoded_email = json_decode($this->get('email'));
 			// Call the transaction_history function in the model
 			$response = $this->AccountTransactionModel->get_transaction_history($decoded_email);
 			// Verify there is something, and respond with the JSON
