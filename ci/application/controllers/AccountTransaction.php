288b5fbaa9d40a2a359979f75568014b326d4f6b
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-13 14:00:58 -0500

    added account transaction info and balance getter functions for HSA and FSA accounts respectively

--- ci/application/controllers/AccountTransaction.php
+++ ci/application/controllers/AccountTransaction.php
@@ -4,10 +4,10 @@ require('application/libraries/REST_Controller.php');
 
 	class AccountTransaction extends REST_Controller {
 
-		// RESTful API to get a user's health account transaction history
-		// Make GET requests to https://capstone.td9175.com/ci/index.php/AccountTransaction/transaction
+		// RESTful API to get a user's HSA transaction history
+		// Make GET requests to https://capstone.td9175.com/ci/index.php/AccountTransaction/hsa_transaction
 		// GET variable to send: email
-		function transaction_get() {
+		function hsa_transaction_get() {
 			// Load the model
 			$this->load->model('AccountTransactionModel');
 			// Check if the email get variable was passed
@@ -17,7 +17,7 @@ require('application/libraries/REST_Controller.php');
 			// URL decode the email
 			$decoded_email = urldecode($this->get('email'));
 			// Call the transaction_history function in the model
-			$response = $this->AccountTransactionModel->get_transaction_history($decoded_email);
+			$response = $this->AccountTransactionModel->get_hsa_transaction_history($decoded_email);
 			// Verify there is something, and respond with the JSON
 			if($response) {
 				$this->response($response, 200);
@@ -25,5 +25,72 @@ require('application/libraries/REST_Controller.php');
 				$this->response(NULL, 404);
 			}
 		}
+
+		// RESTful API to get a user's FSA transaction history
+		// Make GET requests to https://capstone.td9175.com/ci/index.php/AccountTransaction/fsa_transaction
+		// GET variable to send: email
+		function fsa_transaction_get() {
+			// Load the model
+			$this->load->model('AccountTransactionModel');
+			// Check if the email get variable was passed
+			if(!$this->get('email')) {
+				$this->response(NULL, 400);
+			}
+			// URL decode the email
+			$decoded_email = urldecode($this->get('email'));
+			// Call the transaction_history function in the model
+			$response = $this->AccountTransactionModel->get_fsa_transaction_history($decoded_email);
+			// Verify there is something, and respond with the JSON
+			if($response) {
+				$this->response($response, 200);
+			} else {
+				$this->response(NULL, 404);
+			}
+		}
+
+		// Get the balance for a user's HSA account
+		// Make GET requests to https://capstone.td9175.com/ci/index.php/AccountTransaction/hsa_balance
+		// GET variable to send: email
+		function hsa_balance_get() {
+			//Load the model
+			$this->load->model('AccountTransactionModel');
+			// Check if the email get variable was passed
+			if(!$this->get('email')) {
+				$this->response(NULL, 400);
+			}
+			// URL decode the email
+			$decoded_email = urldecode($this->get('email'));
+			// Call the transaction_history function in the model
+			$response = $this->AccountTransactionModel->hsa_balance($decoded_email);
+			// Verify there is something, and respond with the JSON
+			if($response) {
+				$this->response($response, 200);
+			} else {
+				$this->response(NULL, 404);
+			}
+		}
+
+		// Get the balance for a user's FSA account
+		// Make GET requests to https://capstone.td9175.com/ci/index.php/AccountTransaction/fsa_balance
+		// GET variable to send: email
+		function hsa_balance_get() {
+			//Load the model
+			$this->load->model('AccountTransactionModel');
+			// Check if the email get variable was passed
+			if(!$this->get('email')) {
+				$this->response(NULL, 400);
+			}
+			// URL decode the email
+			$decoded_email = urldecode($this->get('email'));
+			// Call the transaction_history function in the model
+			$response = $this->AccountTransactionModel->hsa_balance($decoded_email);
+			// Verify there is something, and respond with the JSON
+			if($response) {
+				$this->response($response, 200);
+			} else {
+				$this->response(NULL, 404);
+			}
+		}
+
 
   }
