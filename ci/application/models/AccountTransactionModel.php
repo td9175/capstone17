46b229aa8ae65b730ef954c5f665e2ec4e7365cb
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-13 14:17:10 -0500

    troubleshooting

--- ci/application/models/AccountTransactionModel.php
+++ ci/application/models/AccountTransactionModel.php
@@ -62,6 +62,7 @@
 			// Build the query string
 			$query = "SELECT sum(A.amount) AS balance FROM AccountTransaction AS A JOIN HealthAccount AS H USING (account_number) WHERE H.email = ? AND H.account_type = 'HSA' ";
 			// Execute the query
+			echo "Email: $email \n \n \n \n";
 			$result = $this->db->query($query, $email);
 			var_dump($result);
 
