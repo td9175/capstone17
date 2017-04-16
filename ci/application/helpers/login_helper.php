de1fc1967087861c672e7da352fcd2b2027354e3
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-16 17:35:31 -0500

    Troubleshooting login_helper.php

--- ci/application/helpers/login_helper.php
+++ ci/application/helpers/login_helper.php
@@ -1,17 +1,16 @@
 <?php
 
-  function is_logged_in() {
-    // $CI =& get_instance();
-    // $user = $CI->session->userdata();
-    // $logged_in = $CI->session->userdata('logged_in');
-    $logged_in = $_SESSION['logged_in'];
-    var_dump($logged_in);
-    // var_dump($user);
-    if (!isset($logged_in)) {
-      return false;
-    } else {
-      return true;
-      }
-    }
+function is_logged_in() {
+  // $CI =& get_instance();
+  // $user = $CI->session->userdata();
+  // $logged_in = $CI->session->userdata('logged_in');
+  if (isset($_SESSION['logged_in'])) {
+    var_dump($_SESSION['logged_in']);
+    return TRUE;
+  } else {
+    return FALSE;
+  }
+
+}
 
  ?>
