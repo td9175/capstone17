3fc0d037556128b747a183c83de2c67d3f05be26
skh6yf <skh6yf@mail.missouri.edu>
2017-04-16 14:26:38 -0500

    testing to see if the @ symbol is the problem

--- ci/application/controllers/OCR.php
+++ ci/application/controllers/OCR.php
@@ -18,7 +18,7 @@ class Ocr extends CI_Controller {
 			//$imagePath = $_SESSION['path'];
 			$request = new Http_Request2('https://westus.api.cognitive.microsoft.com/vision/v1.0/ocr');
 			$url = $request->getUrl();
-			$path = 'https://capstone.td9175.com/ci/application/receipts/walrmart1.jpg';
+			$path = 'https://capstone.td9175.com/ci/application/receipts/umbcapstone17@gmail.com/16-04-170Rs.jpg';
 			//$path .= $imagePath;
 
 			$request->setConfig(array(
