a56ae0d2b8c9a551bc7d89a565dfdee9c6c69c10
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-16 17:32:30 -0500

    Troubleshooting login_helper.php

--- ci/application/config/autoload.php
+++ ci/application/config/autoload.php
@@ -59,8 +59,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-// $autoload['libraries'] = array('database', 'session');
-$autoload['libraries'] = array('database');
+$autoload['libraries'] = array('database', 'session');
 
 /*
 | -------------------------------------------------------------------
--- ci/application/helpers/login_helper.php
+++ ci/application/helpers/login_helper.php
@@ -1,12 +1,13 @@
 <?php
 
   function is_logged_in() {
-    $CI =& get_instance();
-    $user = $CI->session->userdata();
-    $logged_in = $CI->session->userdata('logged_in');
+    // $CI =& get_instance();
+    // $user = $CI->session->userdata();
+    // $logged_in = $CI->session->userdata('logged_in');
+    $logged_in = $_SESSION['logged_in'];
     var_dump($logged_in);
     // var_dump($user);
-    if (!isset($user)) {
+    if (!isset($logged_in)) {
       return false;
     } else {
       return true;
