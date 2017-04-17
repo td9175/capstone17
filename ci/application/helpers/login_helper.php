874693bade171bdff9d6831aef2bedacb2d03fd7
skh6yf <skh6yf@mail.missouri.edu>
2017-04-17 17:43:24 -0500

    debug

--- ci/application/helpers/login_helper.php
+++ ci/application/helpers/login_helper.php
@@ -4,7 +4,8 @@ function is_logged_in() {
   if (isset($_SESSION['logged_in'])) {
     return TRUE;
   } else {
-    die("Please log in.");
+    //die("Please log in.");
+    echo "not logged in"; 
   }
 
 }
