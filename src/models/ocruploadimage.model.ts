ecffa30c1548158cb9b7b453d891194e0d3d77f6
Kenny Clark <kenny.birmanov.clark@gmail.com>
2017-04-17 21:44:35 -0500

    Added a temp ocrUpload model for Ionic
    I may not use this, but it’s a good start.

---
+++ src/models/ocruploadimage.model.ts
@@ -0,0 +1,9 @@
+import { BUNDLER_ROLLUP } from '@ionic/app-scripts/dist/util/constants';
+
+export class OcrUploadImageModel {
+    constructor( 
+        public userfile: string,
+    ) {
+
+    }
+}
