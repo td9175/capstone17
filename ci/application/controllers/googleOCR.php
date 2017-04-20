e51a56bbac8630721c65de95a52accd28007f7ec
skh6yf <skh6yf@mail.missouri.edu>
2017-04-20 18:30:16 -0500

    scrapping google stuff and trying again

--- ci/application/controllers/googleOCR.php
+++ ci/application/controllers/googleOCR.php
@@ -5,6 +5,7 @@
 		@Author: Sami Holder
 		12bit - UMB Bank Health Spending App
 */
+require "vendor/autoload.php";
 header("Access-Control-Allow-Origin: *");
 //require_once(APPPATH.'HTTP_Request2-2.3.0/HTTP/Request2.php');
 
@@ -13,7 +14,7 @@ class googleOCR extends CI_Controller {
 
 
 	public function test_ocr() {
-
+		echo "Testing";
 	}
 	
 	
