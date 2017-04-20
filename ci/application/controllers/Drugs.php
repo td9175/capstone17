8b795e3f058e153d61c4fbb4fcfc4f5e6a22e0a1
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-19 23:46:46 -0500

    Troubleshooting goodrx

--- ci/application/controllers/Drugs.php
+++ ci/application/controllers/Drugs.php
@@ -76,9 +76,10 @@ class Drugs extends CI_Controller {
 
       // Build the query string
 			$name = urldecode($name);
-			$name = urlencode($name);
       $queryString = "name=" . $name . "&api_key=" . $apiKey;
 			echo "$queryString \n";
+
+
 
       // Generate a keyed hash signature using HMAC / SHA256 on the query string and the GoodRx secret API key
       $sig = self::base64url_encode(hash_hmac('sha256', $queryString, $secretKey, true));
