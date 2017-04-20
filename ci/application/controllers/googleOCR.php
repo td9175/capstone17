49dd63d3c40507df9401258b5f379ad0b6f8a23e
skh6yf <skh6yf@mail.missouri.edu>
2017-04-20 18:11:04 -0500

    adding stuff for google vision

--- ci/application/controllers/googleOCR.php
+++ ci/application/controllers/googleOCR.php
@@ -6,13 +6,42 @@
 		12bit - UMB Bank Health Spending App
 */
 header("Access-Control-Allow-Origin: *");
-require_once(APPPATH.'HTTP_Request2-2.3.0/HTTP/Request2.php');
+//require_once(APPPATH.'HTTP_Request2-2.3.0/HTTP/Request2.php');
 
 class googleOCR extends CI_Controller {
 
 
 
+	public function test_ocr() {
+	
+		use GoogleCloudVisionPHP\GoogleCloudVision;
 
+$gcv = new GoogleCloudVision();
+
+// Follow instruction from Google Cloud Vision Document
+$gcv->setKey("AIzaSyBcjXyf318beGjL-Hx1hTr16BK30zKw20I");
+
+$gcv->setImage("receipts/test.png");
+
+// 1 is Max result
+$gcv->addFeature("LABEL_DETECTION", 1);
+
+$gcv->addFeatureUnspecified(1);
+$gcv->addFeatureFaceDetection(1);
+$gcv->addFeatureLandmarkDetection(1);
+$gcv->addFeatureLogoDetection(1);
+$gcv->addFeatureLabelDetection(1);
+$gcv->addFeatureOCR(1);
+$gcv->addFeatureSafeSeachDetection(1);
+$gcv->addFeatureImageProperty(1);
+
+//Optinal
+$gcv->setImageContext(array("languageHints"=>array("th")));
+
+$response = $gcv->request();
+	
+	
+	}
 	
 	
 	
