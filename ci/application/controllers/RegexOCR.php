697dadd18347138f762c084e9ddd0b01d6ae2ba5
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-19 22:19:27 -0500

    Error fix

--- ci/application/controllers/RegexOCR.php
+++ ci/application/controllers/RegexOCR.php
@@ -455,7 +455,7 @@ class RegexOCR extends REST_Controller {
   $yPosition = $matches[1];
 
   foreach ($yPosition as $position) {
-    $regex = '/'. $yPosition . ',.*\n.*text":\s"(.*)"/gi';
+    $regex = '/'. $position . ',.*\n.*text":\s"(.*)"/gi';
     // $regex = '/100,.*\n.*text":\s"(.*)"/gi';
 
     if (preg_match_all($regex, $string, $matches)) {
