4269583774f5f544dfbaab9517301b052d17eeed
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-20 00:05:46 -0500

    Troubleshooting goodrx

--- ci/application/controllers/Drugs.php
+++ ci/application/controllers/Drugs.php
@@ -36,15 +36,15 @@ class Drugs extends CI_Controller {
 
       // Build the query string
       $queryString = "query=" . urldecode($searchQuery) . "&api_key=" . $apiKey;
-			echo "$queryString \n";
+			// echo "$queryString \n";
 
       // Generate a keyed hash signature using HMAC / SHA256 on the query string and the GoodRx secret API key
       $sig = self::base64url_encode(hash_hmac('sha256', $queryString, $secretKey, true));
-			echo "$sig \n";
+			// echo "$sig \n";
 
       //Build the URL string with the query string and keyed hash signature
       $url = $url . $queryString . "&sig=" . $sig;
-			echo "$url \n";
+			// echo "$url \n";
 
       // Set some curl options
       curl_setopt($ch, CURLOPT_URL, $url);
@@ -78,17 +78,17 @@ class Drugs extends CI_Controller {
 			$name = urldecode($name);
 			$name = str_replace(" ", "", $name);
       $queryString = "name=" . $name . "&api_key=" . $apiKey;
-			echo "$queryString \n";
+			// echo "$queryString \n";
 
 
 
       // Generate a keyed hash signature using HMAC / SHA256 on the query string and the GoodRx secret API key
       $sig = self::base64url_encode(hash_hmac('sha256', $queryString, $secretKey, true));
-			echo "$sig \n";
+			// echo "$sig \n";
 
       //Build the URL string with the query string and keyed hash signature
       $url = $url . "name=" . $name . "&api_key=" . $apiKey . "&sig=" . $sig;
-			echo "$url \n";
+			// echo "$url \n";
 
       // Set some curl options
       curl_setopt($ch, CURLOPT_URL, $url);
