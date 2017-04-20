f22e23334d05ff8c97cf3cb88fa0c38eef6e2ba2
skh6yf <skh6yf@mail.missouri.edu>
2017-04-20 18:14:11 -0500

    google vision stuff

--- ci/application/controllers/googleOCR.php
+++ ci/application/controllers/googleOCR.php
@@ -13,34 +13,7 @@ class googleOCR extends CI_Controller {
 
 
 	public function test_ocr() {
-	
-		use GoogleCloudVisionPHP\GoogleCloudVision;
 
-$gcv = new GoogleCloudVision();
-
-// Follow instruction from Google Cloud Vision Document
-$gcv->setKey("AIzaSyBcjXyf318beGjL-Hx1hTr16BK30zKw20I");
-
-$gcv->setImage("receipts/test.png");
-
-// 1 is Max result
-$gcv->addFeature("LABEL_DETECTION", 1);
-
-$gcv->addFeatureUnspecified(1);
-$gcv->addFeatureFaceDetection(1);
-$gcv->addFeatureLandmarkDetection(1);
-$gcv->addFeatureLogoDetection(1);
-$gcv->addFeatureLabelDetection(1);
-$gcv->addFeatureOCR(1);
-$gcv->addFeatureSafeSeachDetection(1);
-$gcv->addFeatureImageProperty(1);
-
-//Optinal
-$gcv->setImageContext(array("languageHints"=>array("th")));
-
-$response = $gcv->request();
-	
-	
 	}
 	
 	
