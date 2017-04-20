e460ab97ae9a6678aff159469bb3b7430660d1e3
skh6yf <skh6yf@mail.missouri.edu>
2017-04-20 15:03:36 -0500

    debugging google view

--- ci/application/views/googleView.php
+++ ci/application/views/googleView.php
@@ -1,14 +1,13 @@
 <?php
 //include_once("creds.php"); // Get $bucket
-require 'application/config/autoload.php';
 use google\appengine\api\cloud_storage\CloudStorageTools;
 //Create Bucket here 
 // https://cloud.google.com/storage/docs/getting-started-console#create_a_bucket
 $bucket = "https://console.cloud.google.com/storage/browser/capstone_receipts/?_ga=1.73457682.1245552135.1492667330&project=capstone-165021";
 
 
-//$options = [ 'gs_bucket_name' => $bucket ];
-//$upload_url = CloudStorageTools::createUploadUrl('/process.php', $options);
+$options = [ 'gs_bucket_name' => $bucket ];
+$upload_url = CloudStorageTools::createUploadUrl('/process.php', $options);
 
 ?>
 
