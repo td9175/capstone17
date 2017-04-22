b494c53b574edcb97baf5113c3a8f7bbef44d5ca
Kenny Clark <kenny.birmanov.clark@gmail.com>
2017-04-22 16:01:00 -0500

    Added Register Page to Src

---
+++ src/pages/register/register.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html'
+})
+export class RegisterPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RegisterPage');
+  }
+
+}
