4665285fc4a878d0095de0e9ade6f131a92fb95c
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-22 16:08:10 -0500

    Regex test

--- ci/application/controllers/RegexOCR.php
+++ ci/application/controllers/RegexOCR.php
@@ -540,7 +540,7 @@ class RegexOCR extends REST_Controller {
   // var_dump($yPosition);
 
   foreach ($yPosition as $position) {
-    echo "$positon \n";
+    echo "$position \n";
   }
 
   // Remove duplicate Y values
