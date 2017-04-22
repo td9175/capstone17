00f2886fb7f29ac30668e8b6a8eb0b26f8e39be9
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-22 17:40:08 -0500

    Regex test

--- ci/application/controllers/RegexOCR.php
+++ ci/application/controllers/RegexOCR.php
@@ -538,8 +538,10 @@ class RegexOCR extends REST_Controller {
   // Put the matches array into a named variable
   $yPositions = $matches[1];
 
+  // Initialize an empty array to hold the positions
   $positions = array();
 
+  // Cast the array of strings to Int
   foreach ($yPositions as $position) {
     $integerPosition = (Int) $position;
     array_push($positions, $integerPosition);
@@ -551,36 +553,20 @@ class RegexOCR extends REST_Controller {
   // Sort ascending
   array_multisort($positions, SORT_ASC);
 
-  // foreach ($positions as $position) {
-  //   echo "$position ";
-  // }
-
-  // preg_replace($regex, $replacement, $positions);
-
-
-  // var_dump($sortedPositions);
-
-
-
-
-
+  // Loop through the Y positions
   foreach ($positions as $position) {
-    $positionPlusOne = $position + 1;
-    $positionMinusOne = $position - 1;
+    // Build regular expression
     $regex = '/\d{1,3},'.$position.',\d{1,3},\d{1,3}.*\n.*text":\s"(.*)"/';
-    // $regex = '/\d{1,3},(?:'.$position.'|'.$positionPlusOne.'|'.$positionMinusOne.'),\d{1,3},\d{1,3}.*\n.*text":\s"(.*)"/';
-    // echo "$regex \n";
 
-    //   /\d{1,3},(?:100|101|99)\d{1,3},\d{1,3}.*\n.*text":\s"(.*)"/
-    //   /\d{1,3},(?:100|101|99),\d{1,3},\d{1,3}.*\n.*text":\s"(.*)"/
-    // $regex = '/100,.*\n.*text":\s"(.*)"/gi';
-
+    // Match for the words
     if (preg_match_all($regex, $string, $matches)) {
-      $lines = $matches[1];
-      // $lines = array_unique($matches[1]);
-      // $words = preg_match_all();
-      foreach ($lines as $line) {
-        echo "$line \n";
+      $words = $matches[1];
+
+      // Match for qualified items, capture the amount
+      preg_match_all($regex, $words, $matches);
+
+      foreach ($words as $word) {
+        echo "$word ";
       }
       // var_dump($matches[1]);
     } else {
