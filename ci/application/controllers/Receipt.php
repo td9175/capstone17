7e5dcbb5bbb39b8fc6681e6ab41d5811935d00b9
skh6yf <skh6yf@mail.missouri.edu>
2017-04-23 15:41:49 -0500

    regex redo

--- ci/application/controllers/Receipt.php
+++ ci/application/controllers/Receipt.php
@@ -66,83 +66,74 @@ function indent($json) {
 
     return $result;
 }
-		function qualified_receipt_regex_post($results) {
-				// Check if a user is logged in
-				//is_logged_in();
+  function qualified_receipt_regex_post($results) {
+    // Check if a user is logged in
+    is_logged_in();
 
-				echo "<br>qualifed_receipt_regex";
-				
-				$string = $results;
-				echo "\nString: " . $string;
+  	$string = $results;
+    // Get the Y cordinate for everything
+    preg_match_all('/\d+,(\d+),\d+,\d+/i', $string, $matches);
 
-				// Get the Y cordinate for everything
-				preg_match_all('/\d+,(\d+),\d+,\d+/i', $string, $matches);
-				
+    // Put the matches array into a named variable
+    $yPositions = $matches[1];
 
-				// Put the matches array into a named variable
-				$yPositions = $matches[1];
+    // Initialize an empty array to hold the positions
+    $positions = array();
 
-				// Initialize an empty array to hold the positions
-				$positions = array();
+    // Cast the array of strings to Int
+    foreach ($yPositions as $position) {
+      $integerPosition = (Int) $position;
+      array_push($positions, $integerPosition);
+    }
 
-				// Cast the array of strings to Int
-				foreach ($yPositions as $position) {
-				  $integerPosition = (Int) $position;
-				  array_push($positions, $integerPosition);
-				}
+    // Remove duplicate Y values
+    $positions = array_unique($positions);
 
-				// Remove duplicate Y values
-				$positions = array_unique($positions);
+    // Sort ascending
+    array_multisort($positions, SORT_ASC);
 
-				// Sort ascending
-				array_multisort($positions, SORT_ASC);
+    $wordString = "";
 
-				$wordString = "";
+    // Loop through the Y positions
+    foreach ($positions as $position) {
+      // Build regular expression
+      $regex = '/\d{1,3},'.$position.',\d{1,3},\d{1,3}.*\n.*text":\s"(.*)"/';
 
-				// Loop through the Y positions
-				foreach ($positions as $position) {
-				  // Build regular expression
-				  	
-				  $regex = '/\d{1,3},'.$position.',\d{1,3},\d{1,3}.*\n.*text":"(.*)"/';
+      // Match for the words
+      preg_match_all($regex, $string, $matches);
+        $words = $matches[1];
 
-				  // Match for the words
-				  preg_match_all($regex, $string, $matches);
-					$words = $matches[1];
-					var_dump($words);
+        // Turn the array into a string
+        foreach ($words as $word) {
+          $wordString .= $word . " ";
+        }
+    }
 
-					// Turn the array into a string
-					foreach ($words as $word) {
-					  $wordString .= $word . " ";
-					  
-					}
-					echo "Wordstring: $wordString\n";
-				}
+    // Match for qualified items, capture the amount
+    $regex = '/([^nxhdjt]\w+\s?\w+)\s?\d{12}H\s(\d+\.\d+)[^\d]/';
+    preg_match_all($regex, $wordString, $matches);
+    $qualifiedItems = $matches[1];
+    $qualifiedAmounts = $matches[2];
 
-				// Match for qualified items, capture the amount
-				$regex = '/\d{1,3},'.$position.',\d{1,3},\d{1,3}.*?text":"(.*?)"/';
-				preg_match_all($regex, $wordString, $matches);
-				$qualifiedItems = $matches[1];
-				$qualifiedAmounts = $matches[2];
+    if (count($qualifiedItems) > 0) {
+      for ($i=0; $i < count($qualifiedItems); $i++) {
+        $response[$i] = array($qualifiedItems[$i] => $qualifiedAmounts[$i]);
+      }
 
-				if (count($qualifiedItems) > 0) {
-				  for ($i=0; $i < count($qualifiedItems); $i++) {
-					$response[$i] = array($qualifiedItems[$i] => $qualifiedAmounts[$i]);
-				  }
-						return $response;
-				  // Add up the amounts for the total qualified amount
-				  // $total = 0;
-				  // foreach ($qualifiedAmounts as $qualifiedAmount) {
-				  //   $total += $qualifiedAmount;
-				  // }
-				  //
-				  // array_push($response, $total);
+      // Add up the amounts for the total qualified amount
+      // $total = 0;
+      // foreach ($qualifiedAmounts as $qualifiedAmount) {
+      //   $total += $qualifiedAmount;
+      // }
+      //
+      // array_push($response, $total);
 
-				} else {
-				  $response = "No reimbursement qualified items.";
-				}
+    } else {
+      $response = "No reimbursement qualified items.";
+    }
 
-				$this->response($response, 200);
-			  }
+    $this->response($response, 200);
+  }
 
 		public function ocr_request() {
 			echo "In OCR request\n";
