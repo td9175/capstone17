4376f9db06c08beb4c7fa42e7611a1c746fb578a
skh6yf <skh6yf@mail.missouri.edu>
2017-04-23 15:54:44 -0500

    making the code pretty

--- ci/application/controllers/Receipt.php
+++ ci/application/controllers/Receipt.php
@@ -16,56 +16,56 @@ require_once(APPPATH.'HTTP_Request2-2.3.0/HTTP/Request2.php');
 	        parent::__construct();
 					$this->load->model('ReceiptModel');
 		}
-function indent($json) {
+			function indent($json) {
 
-    $result      = '';
-    $pos         = 0;
-    $strLen      = strlen($json);
-    $indentStr   = '  ';
-    $newLine     = "\n";
-    $prevChar    = '';
-    $outOfQuotes = true;
+				$result      = '';
+				$pos         = 0;
+				$strLen      = strlen($json);
+				$indentStr   = '  ';
+				$newLine     = "\n";
+				$prevChar    = '';
+				$outOfQuotes = true;
 
-    for ($i=0; $i<=$strLen; $i++) {
+				for ($i=0; $i<=$strLen; $i++) {
 
-        // Grab the next character in the string.
-        $char = substr($json, $i, 1);
+					// Grab the next character in the string.
+					$char = substr($json, $i, 1);
 
-        // Are we inside a quoted string?
-        if ($char == '"' && $prevChar != '\\') {
-            $outOfQuotes = !$outOfQuotes;
+					// Are we inside a quoted string?
+					if ($char == '"' && $prevChar != '\\') {
+						$outOfQuotes = !$outOfQuotes;
 
-        // If this character is the end of an element,
-        // output a new line and indent the next line.
-        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
-            $result .= $newLine;
-            $pos --;
-            for ($j=0; $j<$pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
+					// If this character is the end of an element,
+					// output a new line and indent the next line.
+					} else if(($char == '}' || $char == ']') && $outOfQuotes) {
+						$result .= $newLine;
+						$pos --;
+						for ($j=0; $j<$pos; $j++) {
+							$result .= $indentStr;
+						}
+					}
 
-        // Add the character to the result string.
-        $result .= $char;
+					// Add the character to the result string.
+					$result .= $char;
 
-        // If the last character was the beginning of an element,
-        // output a new line and indent the next line.
-        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
-            $result .= $newLine;
-            if ($char == '{' || $char == '[') {
-                $pos ++;
-            }
+					// If the last character was the beginning of an element,
+					// output a new line and indent the next line.
+					if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+						$result .= $newLine;
+						if ($char == '{' || $char == '[') {
+							$pos ++;
+						}
 
-            for ($j = 0; $j < $pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
+						for ($j = 0; $j < $pos; $j++) {
+							$result .= $indentStr;
+						}
+					}
 
-        $prevChar = $char;
-    }
+					$prevChar = $char;
+				}
 
-    return $result;
-}
+				return $result;
+			}
   function qualified_receipt_regex_post($results) {
     // Check if a user is logged in
     is_logged_in();
@@ -251,12 +251,10 @@ function indent($json) {
 
 				$data['upload_data'] = $this->upload->data('full_path');
 
-				//insert into DB
-				//$path = $this->ReceiptModel->receiptData_post($data['upload_data'], $email);
 
 
 				$path = urlencode($email);
-				//$path = 'umbcapstone17%40gmail.com/';
+
 				$path .= '/';
 				$path .= $f_name;
 				$path .= '.jpg';
@@ -267,7 +265,7 @@ function indent($json) {
 				$parsed = $this->ocr_request();
 				echo "<BR>Parsed data: " . $parsed;
 				if($parsed) {
-					echo "Success\n";
+					
 					$results = $this->qualified_receipt_regex_post($parsed);
 					//echo "Parsed results: " . $results;
 					return $results;
