9c90b1591865b02065b93fb6bd4ad63b21af7928
td9175 <td9175@gmail.com>
2017-04-23 13:32:54 -0500

    Reverting CORS testing

--- ci/application/controllers/HealthAccount.php
+++ ci/application/controllers/HealthAccount.php
@@ -4,8 +4,8 @@
 		12bit - UMB Bank Health Spending App
 */
 
-header("Access-Control-Allow-Origin: http://localhost:8100");
-header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Credentials: true");
 require('application/libraries/REST_Controller.php');
 
 class HealthAccount extends REST_Controller {
@@ -15,7 +15,7 @@ class HealthAccount extends REST_Controller {
   // GET variable: email
   function hsa_get() {
     // Check if a user is logged in
-    is_logged_in();
+    //is_logged_in();
     $this->load->model('HealthAccountModel');
 
     if(!$this->get('email')){
@@ -35,7 +35,7 @@ class HealthAccount extends REST_Controller {
   // POST variables: account_number, email
   function hsa_post() {
     // Check if a user is logged in
-    is_logged_in();
+    //is_logged_in();
     $this->load->model('HealthAccountModel');
 
     if(!$this->post('account_number')){
@@ -56,7 +56,7 @@ class HealthAccount extends REST_Controller {
   // GET variable: email
   function fsa_get() {
     // Check if a user is logged in
-    is_logged_in();
+    //is_logged_in();
     $this->load->model('HealthAccountModel');
 
     if(!$this->get('email')){
@@ -75,7 +75,7 @@ class HealthAccount extends REST_Controller {
   // POST variables: account_number, email
   function fsa_post() {
     // Check if a user is logged in
-    is_logged_in();
+    //is_logged_in();
     $this->load->model('HealthAccountModel');
 
     if(!$this->post('account_number')){
--- ci/application/controllers/UserAccount.php
+++ ci/application/controllers/UserAccount.php
@@ -4,8 +4,8 @@
 		12bit - UMB Bank Health Spending App
 */
 
-header("Access-Control-Allow-Origin: http://localhost:8100");
-header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Credentials: true");
 require('application/libraries/REST_Controller.php');
 
 	// RESTful API for User Account functions
