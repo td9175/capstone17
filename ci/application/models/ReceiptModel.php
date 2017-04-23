ca25441caa124d1cb2a3d5a0a879271daa8a8a56
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-23 16:08:58 -0500

    Fixed error

--- ci/application/models/ReceiptModel.php
+++ ci/application/models/ReceiptModel.php
@@ -23,9 +23,9 @@ class ReceiptModel extends CI_Model {
       if ($result->num_rows() > 0) {
         foreach ($result->result_array() as $row) {
           $data[] = array(
-            'receipt_id' = $row['receipt_id'],
-            'image' = $row['image'],
-            'date_time_stamp' = $row['date_time_stamp']
+            'receipt_id' => $row['receipt_id'],
+            'image' => $row['image'],
+            'date_time_stamp' => $row['date_time_stamp']
           );
         }
       } else {
