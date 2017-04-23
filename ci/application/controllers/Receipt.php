00f1f4a4064b7959d1dc461e3a84b51315ebbe43
skh6yf <skh6yf@mail.missouri.edu>
2017-04-23 13:43:19 -0500

    adding in parsing function to Receipt controller

--- ci/application/controllers/Receipt.php
+++ ci/application/controllers/Receipt.php
@@ -11,7 +11,76 @@ require_once(APPPATH.'HTTP_Request2-2.3.0/HTTP/Request2.php');
 
 	class Receipt extends CI_Controller {
 
+		function qualified_receipt_regex_post($results) {
+				// Check if a user is logged in
+				//is_logged_in();
 
+				echo "<br>qualifed_receipt_regex";
+				$string = $results;
+
+				// Get the Y cordinate for everything
+				preg_match_all('/\d+,(\d+),\d+,\d+/i', $string, $matches);
+
+				// Put the matches array into a named variable
+				$yPositions = $matches[1];
+
+				// Initialize an empty array to hold the positions
+				$positions = array();
+
+				// Cast the array of strings to Int
+				foreach ($yPositions as $position) {
+				  $integerPosition = (Int) $position;
+				  array_push($positions, $integerPosition);
+				}
+
+				// Remove duplicate Y values
+				$positions = array_unique($positions);
+
+				// Sort ascending
+				array_multisort($positions, SORT_ASC);
+
+				$wordString = "";
+
+				// Loop through the Y positions
+				foreach ($positions as $position) {
+				  // Build regular expression
+				  $regex = '/\d{1,3},'.$position.',\d{1,3},\d{1,3}.*\n.*text":\s"(.*)"/';
+
+				  // Match for the words
+				  preg_match_all($regex, $string, $matches);
+					$words = $matches[1];
+
+					// Turn the array into a string
+					foreach ($words as $word) {
+					  $wordString .= $word . " ";
+					}
+				}
+
+				// Match for qualified items, capture the amount
+				$regex = '/([^nxhdjt]\w+\s?\w+)\s?\d{12}H\s(\d+\.\d+)[^\d]/';
+				preg_match_all($regex, $wordString, $matches);
+				$qualifiedItems = $matches[1];
+				$qualifiedAmounts = $matches[2];
+
+				if (count($qualifiedItems) > 0) {
+				  for ($i=0; $i < count($qualifiedItems); $i++) {
+					$response[$i] = array($qualifiedItems[$i] => $qualifiedAmounts[$i]);
+				  }
+
+				  // Add up the amounts for the total qualified amount
+				  // $total = 0;
+				  // foreach ($qualifiedAmounts as $qualifiedAmount) {
+				  //   $total += $qualifiedAmount;
+				  // }
+				  //
+				  // array_push($response, $total);
+
+				} else {
+				  $response = "No reimbursement qualified items.";
+				}
+
+				$this->response($response, 200);
+			  }
 	
 		public function ocr_request() {
 			echo "In OCR request\n";
@@ -60,28 +129,10 @@ require_once(APPPATH.'HTTP_Request2-2.3.0/HTTP/Request2.php');
 
 			try {
 				$response = $request->send();
-				echo $response->getBody();
-				$newanswer = $response->getBody();
-				echo "<br><Br>";
-				return $newanswer;
-		
-
-			   $jsonIterator = new RecursiveIteratorIterator(
-			new RecursiveArrayIterator(json_decode($newanswer, TRUE)),
-			RecursiveIteratorIterator::SELF_FIRST);
-
-			foreach ($jsonIterator as $key => $val) {
-					if(is_array($val)) {
-						echo "$key:";
-						echo "<br>";
-					} else {
-						echo "$key => $val";
-						echo "<br>";
-					}
-			}
-			
-
-
+				$answer = response->getBody();
+				//$newanswer = $response->getBody();
+				//echo "<br><Br>";
+				return $answer;
 
 				}//end try
 			catch (HttpException $ex) {
@@ -161,6 +212,7 @@ require_once(APPPATH.'HTTP_Request2-2.3.0/HTTP/Request2.php');
 				$parsed = $this->ocr_request();
 				if($parsed) {
 					echo "Success\n";
+					$this->qualified_receipt_regex_post($parsed)
 				} else {
 					echo "No results\n";
 				}
