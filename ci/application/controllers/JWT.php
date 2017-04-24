01ce114702cf59e46ca0a18c85002bb182cad09d
td9175 <td9175@gmail.com>
2017-04-24 10:04:53 -0500

    JWT changes

--- ci/application/controllers/JWT.php
+++ ci/application/controllers/JWT.php
@@ -5,7 +5,7 @@ if(isset($_POST['jwtSubject'])) //if the form has been submitted
             $tokenHeader = "{\"alg\": \"HS256\",\"typ\": \"JWT\"}";
             $tokenPayload = "{\"sub\": \"" . $_POST['jwtSubject'] . "\"}";
             $tokenSecret = "SuperDuperSecret2017";
-            $tokenForHash = base64_encode($tokenHeader) . "." . base64_encode($tokenPayload) . "." . $tokenSecret;
+            $tokenForHash = base64_encode($tokenHeader) . "." . base64_encode($tokenPayload) . "." . base64_encode($tokenSecret);
             $token = hash_hmac("sha256",$tokenForHash,$tokenSecret);
 
             echo $tokenHeader . "\n";
