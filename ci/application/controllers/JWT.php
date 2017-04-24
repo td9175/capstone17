ec9591e1e92079db955085dbc6bd5d519096a066
td9175 <td9175@gmail.com>
2017-04-24 11:07:29 -0500

    Test builder

--- ci/application/controllers/JWT.php
+++ ci/application/controllers/JWT.php
@@ -2,6 +2,19 @@
 use Lcobucci\JWT\Builder;
 if(isset($_POST['jwtSubject'])) //if the form has been submitted
 		{
+
+            $builttoken = (new Builder())->setIssuer('http://example.com') // Configures the issuer (iss claim)
+                        ->setAudience('http://example.org') // Configures the audience (aud claim)
+                        ->setId('4f1g23a12aa', true) // Configures the id (jti claim), replicating as a header item
+                        ->setIssuedAt(time()) // Configures the time that the token was issue (iat claim)
+                        ->setNotBefore(time() + 60) // Configures the time that the token can be used (nbf claim)
+                        ->setExpiration(time() + 3600) // Configures the expiration time of the token (nbf claim)
+                        ->set('sub', $_POST['jwtSubject']) // Configures a new claim, called "uid"
+                        ->getToken(); // Retrieves the generated token
+
+
+
+
             $tokenHeader = "{\"alg\": \"HS256\",\"typ\": \"JWT\"}";
             $tokenPayload = "{\"sub\": \"" . $_POST['jwtSubject'] . "\"}";
             $tokenSecret = "superdupersecret2017";
@@ -16,5 +29,6 @@ if(isset($_POST['jwtSubject'])) //if the form has been submitted
             echo $token . "\n";
             echo $tokenFinal . "\n";
             echo "Dale";
+            echo $builttoken;
 		}
 ?>
