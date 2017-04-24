9fe66eef90321d48ad635c47b458e4b13dcbdde6
td9175 <td9175@gmail.com>
2017-04-24 10:59:55 -0500

    Asdfasdf

--- ci/application/controllers/JWT.php
+++ ci/application/controllers/JWT.php
@@ -2,23 +2,19 @@
 //use Lcobucci\JWT\Builder;
 if(isset($_POST['jwtSubject'])) //if the form has been submitted
 		{
-
-            function base64url_encode($data) { 
-                return rtrim(strtr(base64_encode($data), '+/', '-_'), '='); 
-            } 
             $tokenHeader = "{\"alg\": \"HS256\",\"typ\": \"JWT\"}";
             $tokenPayload = "{\"sub\": \"" . $_POST['jwtSubject'] . "\"}";
-            $tokenSecret = "super";
-            $tokenForHash = base64url_encode($tokenHeader) . "." . base64url_encode($tokenPayload);
+            $tokenSecret = "superdupersecret2017";
+            $tokenForHash = base64_encode($tokenHeader) . "." . base64_encode($tokenPayload);
             $token = hash_hmac('sha256',$tokenForHash,$tokenSecret);
             $tokenFinal = $tokenForHash . "." . $token;
 
             echo $tokenHeader . "\n";
-            echo $_POST['jwtSubject'] . "\n";
             echo $tokenPayload . "\n";
             echo $tokenSecret . "\n";
             echo $tokenForHash . "\n";
             echo $token . "\n";
-            echo $tokenFinal;
+            echo $tokenFinal . "\n";
+            echo "Dale";
 		}
 ?>
