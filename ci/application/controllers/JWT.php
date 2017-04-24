0bddede17a188c3eb711db0271aa8f379a106a4d
td9175 <td9175@gmail.com>
2017-04-24 11:08:20 -0500

    Test builder

--- ci/application/controllers/JWT.php
+++ ci/application/controllers/JWT.php
@@ -15,20 +15,20 @@ if(isset($_POST['jwtSubject'])) //if the form has been submitted
 
 
 
-            $tokenHeader = "{\"alg\": \"HS256\",\"typ\": \"JWT\"}";
-            $tokenPayload = "{\"sub\": \"" . $_POST['jwtSubject'] . "\"}";
-            $tokenSecret = "superdupersecret2017";
-            $tokenForHash = base64_encode($tokenHeader) . "." . base64_encode($tokenPayload);
-            $token = hash_hmac('sha256',$tokenForHash,$tokenSecret);
-            $tokenFinal = $tokenForHash . "." . $token;
+            // $tokenHeader = "{\"alg\": \"HS256\",\"typ\": \"JWT\"}";
+            // $tokenPayload = "{\"sub\": \"" . $_POST['jwtSubject'] . "\"}";
+            // $tokenSecret = "superdupersecret2017";
+            // $tokenForHash = base64_encode($tokenHeader) . "." . base64_encode($tokenPayload);
+            // $token = hash_hmac('sha256',$tokenForHash,$tokenSecret);
+            // $tokenFinal = $tokenForHash . "." . $token;
 
-            echo $tokenHeader . "\n";
-            echo $tokenPayload . "\n";
-            echo $tokenSecret . "\n";
-            echo $tokenForHash . "\n";
-            echo $token . "\n";
-            echo $tokenFinal . "\n";
-            echo "Dale";
+            // echo $tokenHeader . "\n";
+            // echo $tokenPayload . "\n";
+            // echo $tokenSecret . "\n";
+            // echo $tokenForHash . "\n";
+            // echo $token . "\n";
+            // echo $tokenFinal . "\n";
+            // echo "Dale";
             echo $builttoken;
 		}
 ?>
