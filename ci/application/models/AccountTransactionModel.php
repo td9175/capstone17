3ee3102fec5c49bd1b47ac5d4513eb4935772ee0
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-23 19:26:46 -0500

    Test

--- ci/application/models/AccountTransactionModel.php
+++ ci/application/models/AccountTransactionModel.php
@@ -140,9 +140,10 @@
 			// Execute the query
 			$result = $this->db->query($query, $email);
 			// Check if any rows were returned
-			// var_dump($result);
-			$num_rows = $result->num_rows();
-			echo "$num_rows \n";
+			var_dump($result);
+
+			// $num_rows = $result->num_rows();
+			// echo "$num_rows \n";
 			if (!empty($num_rows)) {
 				foreach ($result->result_array() as $row) {
 					echo "checkpoint \n";
