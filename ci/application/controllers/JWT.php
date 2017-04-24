1bf20d57e55502773347e03f1af0360ca6d7a52f
td9175 <td9175@gmail.com>
2017-04-24 10:55:47 -0500

    Secret

--- ci/application/controllers/JWT.php
+++ ci/application/controllers/JWT.php
@@ -8,11 +8,11 @@ if(isset($_POST['jwtSubject'])) //if the form has been submitted
             } 
             $tokenHeader = "{\"alg\": \"HS256\",\"typ\": \"JWT\"}";
             $tokenPayload = "{\"sub\": \"" . $_POST['jwtSubject'] . "\"}";
-            $tokenSecret = "superdupersecret2017";
+            $tokenSecret = "super";
             $tokenForHash = base64url_encode($tokenHeader) . "." . base64url_encode($tokenPayload);
             $token = hash_hmac('sha256',$tokenForHash,$tokenSecret);
             $tokenFinal = $tokenForHash . "." . $token;
-            
+
             echo $tokenHeader . "\n";
             echo $_POST['jwtSubject'] . "\n";
             echo $tokenPayload . "\n";
