34cce3525fcabe89eca57f5aff9dcc4d3abc055d
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-23 19:17:11 -0500

    Error checking

--- ci/application/models/AccountTransactionModel.php
+++ ci/application/models/AccountTransactionModel.php
@@ -119,7 +119,7 @@
 			// Execute the query
 			$result = $this->db->query($query, $email);
 			// Check if any rows were returned
-			if ($result->num_rows() === 1) {
+			if ($result->num_rows() > 0) {
 				foreach ($result->result_array() as $row) {
 					$data = $row['balance'];
 				}
@@ -140,7 +140,7 @@
 			// Execute the query
 			$result = $this->db->query($query, $email);
 			// Check if any rows were returned
-			if ($result->num_rows() === 1) {
+			if ($result->num_rows() > 0) {
 				foreach ($result->result_array() as $row) {
 					$data = $row['balance'];
 				}
