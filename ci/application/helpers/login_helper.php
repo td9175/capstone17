39bfb43e43abc14c0057d0b19d1ae9ad10cd8d98
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-24 12:37:27 -0500

    Test

--- ci/application/helpers/login_helper.php
+++ ci/application/helpers/login_helper.php
@@ -20,59 +20,17 @@ function is_logged_in() {
       $token->getHeaders(); // Retrieves the token header
       $token->getClaims(); // Retrieves the token claims
 
-
       $data = new ValidationData(); // It will use the current time to validate (iat, nbf and exp)
       $data->setIssuer('https://capstone.td9175.com');
-      // $data->setIssuer('http://example.com');
-      // $data->setAudience('http://example.org');
-      // $data->setId('4f1g23a12aa');
 
-      var_dump($token->validate($data)); // false, because we created a token that cannot be used before of `time() + 60`
+      if ($token->validate($data) !== TRUE) {
+        die("Valid token is required.");
+      }
 
-      // $data->setCurrentTime(time() + 60); // changing the validation time to future
-      //
-      // var_dump($token->validate($data)); // true, because validation information is equals to data contained on the token
-      //
-      // $data->setCurrentTime(time() + 4000); // changing the validation time to future
-      //
-      // var_dump($token->validate($data)); // false, because token is expired since current time is greater than exp
     } else {
       die("Valid token is required.");
-      // $this->response("Valid token is required.", 400);
     }
 
   }
-
-  // function verifyJWT_post() {
-  //
-  //   if ($this->post('token') !== NULL) {
-  //     $token = $this->post('token');
-  //
-  //     $token = (new Parser())->parse((string) $token); // Parses from a string
-  //     $token->getHeaders(); // Retrieves the token header
-  //     $token->getClaims(); // Retrieves the token claims
-  //
-  //
-  //     $data = new ValidationData(); // It will use the current time to validate (iat, nbf and exp)
-  //     $data->setIssuer('https://capstone.td9175.com');
-  //     // $data->setIssuer('http://example.com');
-  //     // $data->setAudience('http://example.org');
-  //     // $data->setId('4f1g23a12aa');
-  //
-  //     var_dump($token->validate($data)); // false, because we created a token that cannot be used before of `time() + 60`
-  //
-  //     // $data->setCurrentTime(time() + 60); // changing the validation time to future
-  //     //
-  //     // var_dump($token->validate($data)); // true, because validation information is equals to data contained on the token
-  //     //
-  //     // $data->setCurrentTime(time() + 4000); // changing the validation time to future
-  //     //
-  //     // var_dump($token->validate($data)); // false, because token is expired since current time is greater than exp
-  //   } else {
-  //     $this->response("Valid token is required.", 400);
-  //   }
-  //
-  // }
-
 
  ?>
