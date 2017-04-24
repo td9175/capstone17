b5cd36e07dab914ccb3426e9ce241041ebb37f1d
Kenny Clark <kenny.birmanov.clark@gmail.com>
2017-04-23 19:17:36 -0500

    Working on Accounts Detail UI Logic
    Waiting for update from hsa/fsa balance function from backend

--- src/pages/account-detail/account-detail.ts
+++ src/pages/account-detail/account-detail.ts
@@ -19,6 +19,9 @@ export class AccountDetailPage {
   remainingBalance: any;
   transactions: any;
 
+  userHasHsa: boolean;
+  userHasFsa: boolean;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public userApi: UserApi) {
     this.accountType = navParams.get('accountType');
   }
@@ -28,6 +31,86 @@ export class AccountDetailPage {
     this.getTransactionDetails();
   }
 
+  // transaction loads
+
+  loadHSATransaction(){
+    this.userApi.getHsaTransaction().then(
+      data => {
+        if (data == `No HSA account exists.`) {
+          this.userHasHsa = false;
+          console.log('userHasHsa: ', this.userHasHsa);
+        } else {
+          this.userHasHsa = true;
+          this.transactions = data;
+          console.log('HSA Transactions: ', this.transactions)
+          console.log('userHasHsa: ', this.userHasHsa);
+        }
+      },
+    );
+  }
+
+  loadFSATransaction(){
+    this.userApi.getFsaTransaction().then(
+      data => {
+        if (data == `No FSA transaction history exists.`) {
+          this.userHasFsa = false;
+          console.log('userHasFsa: ', this.userHasFsa);
+        } else {
+          this.userHasFsa = true;
+          this.transactions = data;
+          console.log('FSA Transactions: ', this.transactions)
+          console.log('userHasFsa: ', this.userHasFsa);
+        }
+      },
+    );
+  }
+
+  // balance loads
+
+  loadHSABalance(){
+    this.userApi.getHsaBalance().then(
+      data => {
+        if (data == `No HSA account exists.`) {
+          this.userHasHsa = false;
+          console.log('userHasHsa: ', this.userHasHsa);
+        } else {
+          this.userHasHsa = true;
+          this.remainingBalance = data;
+          console.log('HSA Balance: ', this.transactions)
+          console.log('userHasHsa: ', this.userHasHsa);
+        }
+      },
+    );
+  }
+
+  loadFSABalance(){
+    this.userApi.getFsaBalance().then(
+      data => {
+        if (data == `Error: could not calculate FSA account balance.`) {
+          this.userHasFsa = false;
+          console.log('userHasFsa: ', this.userHasFsa);
+        } else {
+          this.userHasFsa = true;
+          this.remainingBalance = data;
+          console.log('FSA Balance: ', this.transactions)
+          console.log('userHasFsa: ', this.userHasFsa);
+        }
+      },
+    );
+  }
+
+  getTransactionDetails(){
+    if (this.accountType == "HSA"){
+      this.loadHSABalance();
+      this.loadHSATransaction();
+    }
+    else{
+      this.loadFSABalance();
+      this.loadFSATransaction();
+    }
+  }
+
+  /*
   getTransactionDetails(){
     if (this.accountType == "HSA"){
       this.userApi.getHsaTransaction().then(data => this.transactions = data);
@@ -38,6 +121,7 @@ export class AccountDetailPage {
       this.userApi.getFsaBalance().then(data => this.remainingBalance = data);
     }
   }
+  */
 
   goBack() {
     this.navCtrl.pop();
