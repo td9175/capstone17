01710ef30e6312be26cf1239eb226aad2539e760
td9175 <td9175@gmail.com>
2017-04-24 10:37:24 -0500

    JWT

--- ci/application/controllers/JWT.php
+++ ci/application/controllers/JWT.php
@@ -2,11 +2,15 @@
 //use Lcobucci\JWT\Builder;
 if(isset($_POST['jwtSubject'])) //if the form has been submitted
 		{
+
+            function base64url_encode($data) { 
+                return rtrim(strtr(base64_encode($data), '+/', '-_'), '='); 
+            } 
             $tokenHeader = "{\"alg\": \"HS256\",\"typ\": \"JWT\"}";
             $tokenPayload = "{\"sub\": \"" . $_POST['jwtSubject'] . "\"}";
             $tokenSecret = "superdupersecret2017";
-            $tokenForHash = base64_encode($tokenHeader) . "." . base64_encode($tokenPayload);
-            $token = hash_hmac("sha256",$tokenForHash,"secret");
+            $tokenForHash = base64url_encode($tokenHeader) . "." . base64url_encode($tokenPayload);
+            $token = hash_hmac("sha256",$tokenForHash,$tokenSecret);
             $tokenFinal = $tokenForHash . "." . $token;
 
             echo $tokenHeader . "\n";
