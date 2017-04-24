3785a78dca935013cebe8ad5e2821b0cd7658674
td9175 <td9175@gmail.com>
2017-04-24 11:02:31 -0500

    Test remove escapes

--- ci/application/controllers/JWT.php
+++ ci/application/controllers/JWT.php
@@ -2,8 +2,8 @@
 //use Lcobucci\JWT\Builder;
 if(isset($_POST['jwtSubject'])) //if the form has been submitted
 		{
-            $tokenHeader = "{\"alg\": \"HS256\",\"typ\": \"JWT\"}";
-            $tokenPayload = "{\"sub\": \"" . $_POST['jwtSubject'] . "\"}";
+            $tokenHeader = "{"alg": "HS256","typ": "JWT"}";
+            $tokenPayload = "{"sub": "" . $_POST['jwtSubject'] . ""}";
             $tokenSecret = "superdupersecret2017";
             $tokenForHash = base64_encode($tokenHeader) . "." . base64_encode($tokenPayload);
             $token = hash_hmac('sha256',$tokenForHash,$tokenSecret);
