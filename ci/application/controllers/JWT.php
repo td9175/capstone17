0234bccd7e86008cec810483508212975d582e4a
td9175 <td9175@gmail.com>
2017-04-24 09:58:47 -0500

    Add jwt

---
+++ ci/application/controllers/JWT.php
@@ -0,0 +1,13 @@
+<?php
+//use Lcobucci\JWT\Builder;
+if(isset($_POST['jwtSubject'])) //if the form has been submitted
+		{
+            $tokenHeader = "{\"alg\": \"HS256\",\"typ\": \"JWT\"}";
+            $tokenPayload = "{\"sub\": \"" . $_POST['jwtSubject'] . "}";
+            $tokenSecret = "SuperDuperSecret2017";
+            $tokenForHash = base64_encode($tokenHeader) . "." . base64_encode($tokenPayload) . "." . $tokenSecret;
+            $token = hash_hmac("sha256",$tokenForHash,$tokenSecret);
+
+            echo $token;
+		}
+?>
